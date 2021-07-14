ce2e95a8e589cef302a44d3cdc7f8b94e4401da3
tusharojhabacancy <tushar.ojha@bacancytechnology.com>
2021-07-14 12:34:34 +0530

    Navigation done of create team member and receipt settings

--- app/src/main/java/com/android/pos/ui/fragments/settings/CustomerReceiptSettings.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/CustomerReceiptSettings.kt
@@ -8,31 +8,22 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.android.pos.R
-import com.android.pos.databinding.FragmentOrderReceiptSettingsBinding
+import com.android.pos.databinding.FragmentCustomerReceiptSettingsBinding
 
-class OrderReceipt : Fragment() {
+class CustomerReceiptSettings : Fragment() {
 
-    private lateinit var binding: FragmentOrderReceiptSettingsBinding
+    private lateinit var binding: FragmentCustomerReceiptSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding =
-            DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_order_receipt_settings,
-                container,
-                false
-            )
+            DataBindingUtil.inflate(inflater, R.layout.fragment_customer_receipt_settings, container, false)
         binding.lifecycleOwner = this
 
-        binding.llCustomerReceiptSettings.setOnClickListener {
-            findNavController().navigate(R.id.action_settings_to_customerReceiptSettings)
-        }
-
-        binding.llKitchenReceiptSettings.setOnClickListener {
-            findNavController().navigate(R.id.action_settings_to_kitchenReceiptSettings)
+        binding.ivBack.setOnClickListener {
+            findNavController().navigateUp()
         }
 
         return binding.root
