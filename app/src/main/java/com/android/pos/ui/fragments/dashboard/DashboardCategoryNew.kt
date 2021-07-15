f9d10869653fb2de393dfa2ae1621f7f3e57f33f
manan1209 <manan.jani@bacancy.com>
2021-07-15 19:16:23 +0530

    dashboard category list new list

---
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryNew.kt
@@ -0,0 +1,33 @@
+package com.android.pos.ui.fragments.dashboard
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.pos.databinding.FragmentDashboardCategoryNewBinding
+
+class DashboardCategoryNew : Fragment() {
+    private lateinit var binding: FragmentDashboardCategoryNewBinding
+
+
+    override fun onAttach(context: Context) {
+            super.onAttach(context)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDashboardCategoryNewBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
