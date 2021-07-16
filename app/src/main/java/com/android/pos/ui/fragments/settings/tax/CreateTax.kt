28a240287ad5b49ba057048078c30cd3d0e9bff9
vishal <vishal.j.patel@bacancy.com>
2021-07-16 15:28:59 +0530

    - Progress dialog small - Create tax after back to list

--- app/src/main/java/com/android/pos/ui/fragments/settings/tax/CreateTax.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/tax/CreateTax.kt
@@ -61,6 +61,15 @@ class CreateTax : Fragment() {
             }
         })
 
+        viewModel.data.observe(viewLifecycleOwner, { event ->
+            event.getContentIfNotHandled()?.let {
+                if (it) {
+                    findNavController().navigateUp()
+                }
+            }
+        })
+
+
     }
 
     /*private fun navigate() {
