2e5e621d76b36276274e9855438adae3c3c002fe
vishal <vishal.j.patel@bacancy.com>
2021-07-23 16:48:47 +0530

    - New Menu UI update

--- app/src/main/java/com/android/pos/ui/dialog/CategoriesDialog.kt
+++ app/src/main/java/com/android/pos/ui/dialog/CategoriesDialog.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.android.pos.R
+import com.android.pos.data.entities.TbCategory
 import com.android.pos.databinding.DialogCategoriesBinding
 import com.android.pos.ui.adapter.CategoriesListAdapter
 import com.android.pos.ui.fragments.inventory.CategoriesViewModel
