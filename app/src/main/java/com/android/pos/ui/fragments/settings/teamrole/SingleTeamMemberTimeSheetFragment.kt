4e40a40f2c9bd1baee26177a85c47be07c229f63
tusharojhabacancy <tushar.ojha@bacancytechnology.com>
2021-07-30 21:30:56 +0530

    Ui for team member time sheet and single member time sheet created

---
+++ app/src/main/java/com/android/pos/ui/fragments/settings/teamrole/SingleTeamMemberTimeSheetFragment.kt
@@ -0,0 +1,27 @@
+package com.android.pos.ui.fragments.settings.teamrole
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.pos.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SingleTeamMemberTimeSheetFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+@AndroidEntryPoint
+class SingleTeamMemberTimeSheetFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_single_team_member_time_sheet, container, false)
+    }
+
+}
