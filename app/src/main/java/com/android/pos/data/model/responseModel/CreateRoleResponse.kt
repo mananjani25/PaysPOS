671574c450ff19ea7e59073f591bc49399c29536
tusharojhabacancy <tushar.ojha@bacancytechnology.com>
2021-08-06 12:35:35 +0530

    Response changed of create role

--- app/src/main/java/com/android/pos/data/model/responseModel/CreateRoleResponse.kt
+++ app/src/main/java/com/android/pos/data/model/responseModel/CreateRoleResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class CreateRoleResponse(
     @SerializedName("data")
-    val `data`: List<Data>,
+    val `data`: Data,
     @SerializedName("message")
     val message: String,
     @SerializedName("status")
@@ -14,32 +14,37 @@ data class CreateRoleResponse(
     val type: String
 ) {
     data class Data(
-        @SerializedName("employees")
-        val employees: List<Employee>,
-        @SerializedName("id")
-        val id: Int,
-        @SerializedName("name")
-        val name: String
+        @SerializedName("team_roles")
+        val teamRoles: List<TeamRole>
     ) {
-        data class Employee(
-            @SerializedName("email")
-            val email: String,
-            @SerializedName("first_name")
-            val firstName: String,
+        data class TeamRole(
+            @SerializedName("employees")
+            val employees: List<Employee>,
             @SerializedName("id")
             val id: Int,
-            @SerializedName("is_active")
-            val isActive: Boolean,
-            @SerializedName("last_name")
-            val lastName: String,
-            @SerializedName("location_id")
-            val locationId: Int,
             @SerializedName("name")
-            val name: String,
-            @SerializedName("passcode")
-            val passcode: String,
-            @SerializedName("phone_number")
-            val phoneNumber: String
-        )
+            val name: String
+        ) {
+            data class Employee(
+                @SerializedName("email")
+                val email: String,
+                @SerializedName("first_name")
+                val firstName: String,
+                @SerializedName("id")
+                val id: Int,
+                @SerializedName("is_active")
+                val isActive: Boolean,
+                @SerializedName("last_name")
+                val lastName: String,
+                @SerializedName("location_id")
+                val locationId: Int,
+                @SerializedName("name")
+                val name: String,
+                @SerializedName("passcode")
+                val passcode: String,
+                @SerializedName("phone_number")
+                val phoneNumber: String
+            )
+        }
     }
 }
