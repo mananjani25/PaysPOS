7e5a2feda33e61ecf134d888dd2671ff08ff1828
tusharojhabacancy <tushar.ojha@bacancytechnology.com>
2021-08-11 21:48:32 +0530

    Timesheet api in progress

--- app/src/main/java/com/android/pos/data/db/AppDatabase.kt
+++ app/src/main/java/com/android/pos/data/db/AppDatabase.kt
@@ -24,7 +24,7 @@ import com.android.pos.data.typeconvert.*
         GetServiceChargeResponse.Data::class, Employee::class, CartModel::class,
         CustomerListResponse.Data::class, ModifierSet::class, TeamRole::class, ItemModifierSet::class,
         ModulePermission::class],
-    version = 1
+    version = 2
 )
 @TypeConverters(
     TypeConvertersIds::class,
