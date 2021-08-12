5d581a993fbada0f24999053abba1cae5af575ce
vishal <vishal.j.patel@bacancy.com>
2021-08-12 20:17:14 +0530

    - Service charge added in cart table  - Order Request model add  - API added in order (Working...)

--- app/src/main/java/com/android/pos/data/db/AppDatabase.kt
+++ app/src/main/java/com/android/pos/data/db/AppDatabase.kt
@@ -7,13 +7,10 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.android.pos.data.dao.*
 import com.android.pos.data.entities.*
-import com.android.pos.data.entities.CartModel
-import com.android.pos.data.entities.ModifierSet
-import com.android.pos.data.entities.TbCategory
-import com.android.pos.data.entities.TbItem
 import com.android.pos.data.model.CharacterModel
 import com.android.pos.data.model.CustomerListResponse
-import com.android.pos.data.model.responseModel.*
+import com.android.pos.data.model.responseModel.GetTipReponse
+import com.android.pos.data.model.responseModel.NoteResponse
 import com.android.pos.data.remote.Constants.DATABASE_NAME
 import com.android.pos.data.typeconvert.*
 
@@ -24,7 +21,7 @@ import com.android.pos.data.typeconvert.*
         TbServiceCharge::class, Employee::class, CartModel::class,
         CustomerListResponse.Data::class, ModifierSet::class, TeamRole::class, ItemModifierSet::class,
         ModulePermission::class],
-    version = 2
+    version = 1
 )
 @TypeConverters(
     TypeConvertersIds::class,
@@ -33,7 +30,8 @@ import com.android.pos.data.typeconvert.*
     TypeConvertersEmployee::class,
     TCModifier::class,
     TypeConvertersTax::class,
-    TypeConvertersModule::class
+    TypeConvertersModule::class,
+    TCServiceCharge::class
 )
 
 
