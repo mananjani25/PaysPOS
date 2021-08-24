b44801eed412e6a1ed7226736a88aa90703f3a45
manan1209 <manan.jani@bacancy.com>
2021-08-24 22:36:06 +0530

    upto save button add in manual sale

--- app/src/main/java/com/android/pos/data/model/PrinterListModel.kt
+++ app/src/main/java/com/android/pos/data/model/PrinterListModel.kt
@@ -1,11 +1,13 @@
 package com.android.pos.data.model
 
 import com.android.pos.data.remote.Constants.CUSTOMER
+import com.epson.epsonio.DeviceInfo
 
 data class PrinterListModel(
     val id: Int? = null,
     var printerName: String? = null,
     var connectionType: String? =null,
     var isActive: Boolean = false,
-    val type: String = CUSTOMER
+    val type: String = CUSTOMER,
+    var deviceModel: DeviceInfo
 )
