da89d6436fd2ff0e0d42cc817dc20a1c88a62a27
manan1209 <manan.jani@bacancy.com>
2021-10-05 22:39:25 +0530

    - Swipe Helper in Dine In Working

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryNew.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryNew.kt
@@ -294,7 +294,7 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
         binding.rvOrderType.adapter = orderTypeAdapter
 
         viewModel.orderTypes().observe(requireActivity(), {
-            Log.e("ORDER_TYPE_SIZE", it.data?.size.toString())
+            Log.e("ORDER_TYPE_SIZE", "${Gson().toJson(it.data)}")
             it.data?.let { it1 -> orderTypeAdapter.addAll(it1) }
         })
 
@@ -303,7 +303,6 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
     private fun getServiceCharges() {
 
         viewModel.serviceCharges.observe(requireActivity(), {
-            Log.e(TAG, "ServiceChargeListSize: ${it.data?.size}")
             serviceChargesList = it.data
             getCartList()
         })
@@ -1380,7 +1379,10 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
 
 
         if (variation != null) {
-            data.price = variation.price!!
+            variation.price?.let {
+                data.price = it
+            }
+
         } else {
             data.price = data.price
         }
@@ -1511,6 +1513,9 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
                 hideOrderType()
             }
             DINE_IN -> {
+                prefProvider.setValueInt(ORDER_TYPE_ID, orderType.id)
+                prefProvider.setValue(ORDER_TYPE_NAME, orderType.name)
+                prefProvider.setValue(ORDER_TYPE, orderType.orderType)
                 findNavController().navigate(
                     R.id.action_dashboardCategoryNew_to_dineInFragment
                 )
@@ -1644,10 +1649,15 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
                         bundle.putString("orderOfflineId", orderOfflineId)
                     }
 
-                    findNavController().navigate(
-                        R.id.action_dashboardCategoryNew_to_paymentFragment,
-                        bundle
-                    )
+                    if (prefProvider.getValue(ORDER_TYPE, "").toString() == DINE_IN) {
+
+                    } else {
+
+                        findNavController().navigate(
+                            R.id.action_dashboardCategoryNew_to_paymentFragment,
+                            bundle
+                        )
+                    }
 
                 }
             }
@@ -1839,6 +1849,13 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
             cartList.add(cartModel)
         }
 
+        /* Log.e(TAG, "OrderId:   ${orderType!!.id}")
+         Log.e(TAG, "OrderName:   ${orderType!!.name}")
+         Log.e(TAG, "OrderOrderType:   ${orderType!!.orderType}")
+         prefProvider.setValueInt(ORDER_TYPE_ID, orderType!!.id)
+         prefProvider.setValue(ORDER_TYPE_NAME, orderType!!.name)
+         prefProvider.setValue(ORDER_TYPE, orderType!!.orderType)
+ */
         cartList.get(0).orderType = DINE_IN
         viewModel.cartLogic(cartList, null, ADD, dineInList = dineInList)
 
@@ -2160,6 +2177,34 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
             )
         }
 
+
+    }
+
+    override fun onItemDelete(position: Int, itemPosition: Int, data: TbItem) {
+        Log.e(TAG, "ItemDineDeleteHeader ${position}")
+        Log.e(TAG, "ItemDineDelete ${itemPosition}")
+
+        alert(
+            getString(R.string.app_name),
+            getString(R.string.delete_item_message)
+        ) {
+            positiveButton(getString(R.string.tv_delete)) {
+                // Do positive stuff here
+                cartList.get(0).orderType = DINE_IN
+
+                viewModel.cartLogic(
+                    cartList,
+                    data,
+                    DELETE,
+                    dineInList = dineInCartAdapter.getList()
+                )
+
+
+            }
+            negativeButton(R.string.tv_cancel) {
+                // Do negative stuff here
+            }
+        }
 
     }
 
