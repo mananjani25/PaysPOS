cf9139f0e765dc70e6cf0027084fe71cf92947fe
manan1209 <manan.jani@bacancy.com>
2021-10-06 18:59:18 +0530

    - Date and Time Format in Receipt UI and Print

--- app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -533,7 +533,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
 
                         val current = LocalDateTime.now()
-                        val formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:a")
+                        val formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy HH:mm:a")
                         val formatted = current.format(formatter)
 
                         builder.addTextLineSpace(30)
@@ -658,7 +658,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
 
                         val current = LocalDateTime.now()
-                        val formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:a")
+                        val formatter = DateTimeFormatter.ofPattern("MMM-dd-yyyy HH:mm:a")
                         val formatted = current.format(formatter)
                         builder.addTextLineSpace(30)
                         builder.addFeedUnit(30)
