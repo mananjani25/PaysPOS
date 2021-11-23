a6303dfb39791eaef913e37df15279da04b5f7cd
vishal <vishal.j.patel@bacancy.com>
2021-11-23 19:42:09 +0530

    - merged code loyalty points

--- app/src/main/java/com/android/pos/ui/fragments/settings/loyaltypoints/CreateLoyaltyFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/loyaltypoints/CreateLoyaltyFragment.kt
@@ -46,9 +46,9 @@ class CreateLoyaltyFragment : Fragment() {
         isEdit = arguments?.getBoolean("isEdit")!!
 
         if (isEdit) {
-            binding.txtHeader.text = getString(R.string.create_loyalty_point)
+
             binding.txtSave.text = getString(R.string.update)
-            binding.txtLoyaltyPointsTitle.text = getString(R.string.update_loyalty_point)
+            binding.txtHeader.text = getString(R.string.update_loyalty_point)
             loyaltyProgramsModel = arguments?.getParcelable("loyaltyObject")
 
             viewModel.setLoyaltyData(loyaltyProgramsModel)
@@ -57,7 +57,7 @@ class CreateLoyaltyFragment : Fragment() {
                 loyaltyProgramsModel?.rewardType == getString(R.string.percentage_symbol)
 
         } else {
-            binding.txtHeader.text = getString(R.string.edit_loyalty_point)
+            binding.txtHeader.text = getString(R.string.create_loyalty_point)
             binding.swtCreateLoyalty.isChecked = true
         }
         discountType(binding.swtCreateLoyalty.isChecked)
