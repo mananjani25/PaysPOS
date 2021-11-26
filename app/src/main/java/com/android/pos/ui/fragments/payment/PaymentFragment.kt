dce0971fb846f4e79d1ae8cf58a1c47bcd5fb18a
vishal <vishal.j.patel@bacancy.com>
2021-11-26 18:31:17 +0530

    paymentDialog add cashhdiscount

--- app/src/main/java/com/android/pos/ui/fragments/payment/PaymentFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/payment/PaymentFragment.kt
@@ -500,7 +500,7 @@ open class PaymentFragment : Fragment(), View.OnClickListener {
 
                     isSplitByNo -> {
 
-                        (totalPrice / splitValue) + tipAmount
+                        ((totalPrice - final_discount) / splitValue) + tipAmount
 
                     }
                     isSplitByAmount -> {
@@ -815,7 +815,7 @@ open class PaymentFragment : Fragment(), View.OnClickListener {
                 when {
                     isSplitByNo -> {
 
-                        var payAmount = ((totalPrice / splitValue) + tipAmount)
+                        var payAmount = (((totalPrice - final_discount) / splitValue) + tipAmount)
                         val bundle = Bundle()
                         bundle.putDouble("totalPrice", payAmount)
                         bundle.putDouble("paymentAmount", paymentAmount)
@@ -823,7 +823,10 @@ open class PaymentFragment : Fragment(), View.OnClickListener {
                         bundle.putParcelable("receiptData", it.data)
                         bundle.putBoolean("isSpilt", true)
                         bundle.putInt("splitValue", splitValue)
-                        bundle.putDouble("remainingAmount", (totalPrice) - (payAmount - tipAmount))
+                        bundle.putDouble(
+                            "remainingAmount",
+                            (totalPrice - final_discount) - (payAmount - tipAmount)
+                        )
                         bundle.putDouble("payAmount", payAmount)
                         findNavController().navigate(
                             R.id.action_paymentFragment_to_orderCompleteFragment,
