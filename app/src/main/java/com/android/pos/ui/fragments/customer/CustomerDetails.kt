178d0e1401e50a6bb672360e44aa98492e50fec8
Naisargi <naisargi.patel@bacancy.com>
2021-12-03 14:02:29 +0530

    reorder

--- app/src/main/java/com/android/pos/ui/fragments/customer/CustomerDetails.kt
+++ app/src/main/java/com/android/pos/ui/fragments/customer/CustomerDetails.kt
@@ -7,12 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.android.pos.R
-import com.android.pos.data.entities.TbCustomer
+import com.android.pos.data.entities.*
+import com.android.pos.data.model.responseModel.GetOrderDetailsResponse
+import com.android.pos.data.remote.Constants
 import com.android.pos.databinding.FragmentCustomerDetailsBinding
+import com.android.pos.di.PrefProvider
 import com.android.pos.ui.adapter.OrderHistoryAdapter
+import com.android.pos.ui.fragments.dashboard.DashBoardCategoryViewModel
 import com.android.pos.utils.AlertUtils
 import com.android.pos.utils.EventObserver
 import com.android.pos.utils.ProgressUtils
@@ -22,6 +27,7 @@ import com.android.pos.utils.extensions.visible
 import com.google.android.material.snackbar.Snackbar
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CustomerDetails : Fragment() {
@@ -29,12 +35,22 @@ class CustomerDetails : Fragment() {
     private lateinit var binding: FragmentCustomerDetailsBinding
     lateinit var customerModel: TbCustomer
     val TAG = "CustomerDetails"
+
     private val viewModel by viewModels<CustomerListViewModel>()
+    private val dashboardViewModel by activityViewModels<DashBoardCategoryViewModel>()
+
+    @Inject
+    lateinit var prefProvider: PrefProvider
+
     private val orderHistoryAdapter by lazy {
         OrderHistoryAdapter { view, order ->
             //order
-            val bundle: Bundle = bundleOf("orderId" to "${order.id}")
-            findNavController().navigate(R.id.action_customer_to_dashboardCategoryNew, bundle)
+            order.id?.let {
+                viewModel.apiCallOrderDetails(orderId = order.id)
+                val bundle: Bundle = bundleOf("orderId" to "${order.id}")
+                //findNavController().navigate(R.id.action_customer_to_dashboardCategoryNew, bundle)
+            } ?: viewModel.showError(getString(R.string.error_order_id_not_available))
+
         }
     }
 
@@ -147,5 +163,230 @@ class CustomerDetails : Fragment() {
                 binding.llOrderHistory.gone()
             }
         })
+        viewModel.orderResponse.observe(viewLifecycleOwner, EventObserver { order ->
+            //reorder
+            prefProvider.setValue(Constants.ORDER_TYPE, order.orderType)
+            Log.e("!_@_","customer details ${order.orderType}")
+            if (order.customer != null) {
+                prefProvider.setValue(
+                    Constants.CUSTOMER_NAME,
+                    order.customer.firstName + " " + order.customer.lastName
+                )
+                prefProvider.saveCustomerData(TbCustomer.customerMapping(order.customer))
+            }
+
+            dashboardViewModel.addCart(
+                cartModel(order)
+            )
+            val bundle = Bundle()
+            bundle.putBoolean("update", false)
+            bundle.putBoolean("reorder", true)
+            findNavController().navigate(
+                R.id.action_customer_to_dashboardCategoryNew, bundle
+            )
+        })
+    }
+
+    private fun cartModel(order: GetOrderDetailsResponse.Data): CartModel {
+        return CartModel().apply {
+            terminalId = order.terminalId
+            employeeID = order.employeeId
+            locationId = order.locationId
+            orderTypeId = order.orderTypeId
+            orderType = order.orderType
+            orderTypeName = order.orderType
+            futureDeliveryDate = order.date
+            isOpenOrder = false
+            serviceCharge = serviceChargesList(order)
+            customer = assignCustomer(order)
+            items = inventoryList(order)
+            note = order.note
+            var itemDiscount = 0.0
+            items?.forEach {
+                itemDiscount += it.discountPrice
+            }
+            discountPrice = (order.totalDiscount - itemDiscount)
+        }
+    }
+
+    private fun serviceChargesList(order: GetOrderDetailsResponse.Data): List<TbServiceCharge> {
+
+        val serviceChargeList = ArrayList<TbServiceCharge>()
+
+        order.orderServiceCharges.forEach {
+            val serviceCharge = TbServiceCharge(
+                it.createdAt,
+                it.serviceChargeId,
+                true,
+                order.locationId,
+                it.name,
+                it.rate,
+                it.updatedAt,
+                isActive = false,
+                isChecked = true,
+                order_service_charge_id = it.id
+            )
+            serviceChargeList.add(serviceCharge)
+        }
+
+        return serviceChargeList
+    }
+
+    private fun assignCustomer(order: GetOrderDetailsResponse.Data): TbCustomer {
+
+        val phoneList = ArrayList<TbPhones>()
+        order.customer?.phones?.forEach {
+            val phone = TbPhones(it.id, it.phoneNumber)
+            phoneList.add(phone)
+        }
+
+        val addressList = ArrayList<TbAddress>()
+        order.customer?.addresses?.forEach {
+            val address = TbAddress(
+                it.id,
+                it.address1,
+                it.address2,
+                it.city,
+                it.state,
+                it.country ?: "",
+                it.postcode ?: "",
+                "",
+                it.latitude ?: "",
+                it.longitude ?: "",
+                "",
+                it.fullAddress,
+                it.street
+            )
+            addressList.add(address)
+        }
+
+        return TbCustomer(
+            order.customer?.id,
+            order.customer?.firstName?.toString(),
+            order.customer?.lastName?.toString(),
+            order.customer?.birthDate?.toString(),
+            order.customer?.email?.toString(),
+            false,
+            0,
+            order.customer?.company?.toString(),
+            phoneList,
+            addressList
+        )
+    }
+
+    private fun inventoryList(order: GetOrderDetailsResponse.Data): List<TbItem>? {
+
+        val inventoryModelList = ArrayList<TbItem>()
+
+        order.orderItems.forEach {
+
+            val items = TbItem().apply {
+                orderItemId = it.id
+                itemId = it.itemId
+                name = it.itemName
+                cost = it.price
+                price = it.price
+                priceType = ""
+                itemQuantity = it.quantity
+                kitchenName = ""
+                productCode = ""
+                sku = ""
+                isHide = false
+                sort = 0
+                imageUrl = ""
+                thumbImageUrl = ""
+                categoryId = it.categoryId
+                categoryName = ""
+                taxes = taxes(it.orderItemTaxes, order.locationId)
+                modifier_set_ids = modifiersIds(it.orderItemModifiers)
+                modifiers = modifierSets(it.orderItemModifiers)
+                discountPrice = it.discountAmount
+                discountType = it.discountType
+                if (it.discountId != null)
+                    discountId = it.discountId
+                if (it.order_item_variation != null)
+                    variationsAttributes = variationAtt(it.order_item_variation)
+                note = it.note
+            }
+
+            inventoryModelList.add(items)
+
+        }
+
+        return inventoryModelList
+    }
+
+    private fun variationAtt(variation: GetOrderDetailsResponse.Data.OrderItem.OrderItemVariationAttribute?): List<VariationsAttribute> {
+
+        val variationsAttributeList = ArrayList<VariationsAttribute>()
+
+        val variationsAttribute = VariationsAttribute()
+        variationsAttribute.id = variation?.variationId
+        variationsAttribute.name = variation?.name?:""
+        variationsAttribute.price = variation?.price
+        variationsAttribute.orderVariationId = variation?.id
+        variationsAttributeList.add(variationsAttribute)
+
+        return variationsAttributeList
+    }
+
+    private fun taxes(
+        taxs: List<GetOrderDetailsResponse.Data.OrderItem.OrderItemTaxe>,
+        locationId: Int
+    ): List<TaxData>? {
+        val taxList = ArrayList<TaxData>()
+
+        taxs.forEach {
+            val tax = TaxData(
+                it.createdAt,
+                it.taxId,
+                locationId,
+                it.name,
+                it.rate,
+                "Percentage",
+                it.updatedAt,
+                true,
+                it.isDefault,
+                false,
+                "",
+                listOf(),
+                it.id
+            )
+            taxList.add(tax)
+        }
+
+        return taxList
+    }
+
+    private fun modifierSets(orderItemModifiers: List<GetOrderDetailsResponse.Data.OrderItem.OrderItemModifier>): List<Modifier> {
+
+        val modifierList = ArrayList<Modifier>()
+
+        orderItemModifiers.forEach {
+
+            val modifier = Modifier().apply {
+                id = it.modifierId?.toInt() ?: 0
+                //modifierSetId = it.modifierSetId
+                name = it.name
+                price = it.price
+                itemQuantity = it.quantity
+                orderModifierId = it.id
+
+            }
+            modifierList.add(modifier)
+        }
+
+        return modifierList
+    }
+
+    private fun modifiersIds(orderItemModifiers: List<GetOrderDetailsResponse.Data.OrderItem.OrderItemModifier>): List<Int> {
+
+        val selectedIds = ArrayList<Int>()
+        if (orderItemModifiers.isNotEmpty()) {
+            orderItemModifiers.forEach {
+                selectedIds.add(it.id)
+            }
+        }
+        return selectedIds
     }
 }
--- app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryNew.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryNew.kt
@@ -104,6 +104,7 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
     private var orderId: Int? = null
     private var paymentId: Int? = null
     private var isOrderUpdate: Boolean = false
+    private var isReOrder: Boolean = false
     private var future_delivery_time: String = ""
     private var popupWindow: PopupWindow? = null
     private var orderType: TbOrderType? = null
@@ -184,6 +185,8 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
             }
 
         }
+
+        isReOrder = requireArguments().getBoolean("reorder")
 
         viewModel.getCashDiscountDetails(active = 1)
             ?.observe(viewLifecycleOwner, { cashDiscountData ->
@@ -517,6 +520,7 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
             future_delivery_time = bundle.getString("TIME").toString()
             prefProvider.setValueInt(ORDER_TYPE_ID, orderType!!.id)
             prefProvider.setValue(ORDER_TYPE_NAME, orderType!!.name)
+            Log.e("!_@_","523 ${orderType!!.orderType}")
             prefProvider.setValue(ORDER_TYPE, orderType!!.orderType)
             hideOrderType()
         }
@@ -685,10 +689,10 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
                             binding.layoutCart.txtTotalAmount, getDiscountCashData()
                         )
 
-                        if (prefProvider.getValue(ORDER_TYPE, "")
-                                .toString() == TAKEOUT || prefProvider.getValue(ORDER_TYPE, "")
-                                .toString() == Constants.DINE_IN
-                        ) {
+                        val orderType = prefProvider.getValue(ORDER_TYPE, "")
+                        Log.e("!_@_","rlSave -------- $orderType ")
+                        if (orderType == TAKEOUT || orderType == Constants.DINE_IN) {
+                            Log.e("!_@_","rlSave -- GONE ")
                             binding.layoutCart.rlSave.visibility = View.GONE
                             if (prefProvider.getValue(ORDER_TYPE, "").toString() == DINE_IN) {
                                 binding.layoutCart.txtTotalAmount.visibility = View.GONE
@@ -703,6 +707,9 @@ class DashboardCategoryNew : Fragment(), CategoryItemAdapter1.CategoryItemList, 
                             } else {
                                 binding.layoutCart.txtDineInProceed.visibility = View.GONE
                             }
+                        } else {
+                            Log.e("!_@_","rlSave -- VISIBLE ")
+                            binding.layoutCart.rlSave.visibility = View.VISIBLE
                         }
 
                         if (prefProvider.getValueInt("ORDER_ID", -1) != -1) {
