bc880671d78a2af06821cbdabdf83152623735fd
vishal <vishal.j.patel@bacancy.com>
2021-12-07 12:19:14 +0530

    time filter with dollor symbol added in team member

--- app/src/main/java/com/android/pos/ui/fragments/settings/teamrole/TeamMemberTimeSheetFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/teamrole/TeamMemberTimeSheetFragment.kt
@@ -1,6 +1,7 @@
 package com.android.pos.ui.fragments.settings.teamrole
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -21,13 +22,16 @@ import com.android.pos.data.entities.TeamRole
 import com.android.pos.data.remote.Constants
 import com.android.pos.databinding.FragmentTeamMemberTimeSheetBinding
 import com.android.pos.ui.adapter.TeamMemberTimeSheetAdapter
+import com.android.pos.utils.AlertUtils
 import com.android.pos.utils.ProgressUtils
 import com.android.pos.utils.extensions.liveSnackBar
 import com.android.pos.utils.extensions.showAlert
 import com.android.pos.utils.statusUtils.Status
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.abs
 
 @AndroidEntryPoint
 class TeamMemberTimeSheetFragment : Fragment(), AdapterView.OnItemSelectedListener {
@@ -37,9 +41,13 @@ class TeamMemberTimeSheetFragment : Fragment(), AdapterView.OnItemSelectedListen
     private val viewModel by viewModels<TeamMemberSheetViewModel>()
     private lateinit var startDate: DatePickerDialog.OnDateSetListener
     private lateinit var endDate: DatePickerDialog.OnDateSetListener
+    private lateinit var startTime: TimePickerDialog.OnTimeSetListener
+    private lateinit var endTime: TimePickerDialog.OnTimeSetListener
     private var teamRoleListGlobal: ArrayList<TeamRole> = arrayListOf()
     val myCalendar = Calendar.getInstance()
     val myCalendar1 = Calendar.getInstance()
+    val myCalendar2 = Calendar.getInstance()
+    val myCalendar3 = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -97,19 +105,61 @@ class TeamMemberTimeSheetFragment : Fragment(), AdapterView.OnItemSelectedListen
             myCalendar.set(Calendar.MONTH, monthOfYear)
             myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
-            viewModel.updateLabel(myCalendar)
-            viewModel.apiCallTimeSheet(getRoleId(binding.includeView.spRoles.selectedItemPosition).toString())
+            TimePickerDialog(
+                requireActivity(),
+                startTime,
+                myCalendar2.get(Calendar.HOUR),
+                myCalendar2.get(Calendar.MINUTE),
+                false
+            ).show()
+
 
         }
+        startTime = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+            val timecalender = Calendar.getInstance()
+            timecalender.set(Calendar.HOUR_OF_DAY, hour)
+            timecalender.set(Calendar.MINUTE, minute)
+            viewModel.startDate.value = timeCalculateForStartEndTime(hour, minute, "isstart")
+            if (differnceTrue(viewModel.startDate.value!!, viewModel.endDate.value) <= 30) {
+                viewModel.apiCallTimeSheet(getRoleId(binding.includeView.spRoles.selectedItemPosition).toString())
+            } else {
+                AlertUtils.showCustomAlertWithListenerWithOK(
+                    requireActivity(),
+                    "Please Select date in 30 Days."
+                ) { _, _ ->
+                }
+            }
+        }
+
+        endTime = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+            val timecalender = Calendar.getInstance()
+            timecalender.set(Calendar.HOUR_OF_DAY, hour)
+            timecalender.set(Calendar.MINUTE, minute)
+            viewModel.endDate.value = timeCalculateForStartEndTime(hour, minute, "isend")
+            if (differnceTrue(viewModel.endDate.value!!, viewModel.startDate.value) <= 30) {
+                viewModel.apiCallTimeSheet(getRoleId(binding.includeView.spRoles.selectedItemPosition).toString())
+            } else {
+                AlertUtils.showCustomAlertWithListenerWithOK(
+                    requireActivity(),
+                    "Please Select date in 30 Days."
+                ) { _, _ ->
+                }
+            }
+
+        }
+
 
         endDate = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
             myCalendar1.set(Calendar.YEAR, year)
             myCalendar1.set(Calendar.MONTH, monthOfYear)
             myCalendar1.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-            viewModel.updateLabel(myCalendar1)
-            viewModel.apiCallTimeSheet(getRoleId(binding.includeView.spRoles.selectedItemPosition).toString())
-
+            TimePickerDialog(
+                requireActivity(),
+                endTime,
+                myCalendar3.get(Calendar.HOUR),
+                myCalendar3.get(Calendar.MINUTE),
+                false
+            ).show()
         }
 
         return binding.root
@@ -119,7 +169,81 @@ class TeamMemberTimeSheetFragment : Fragment(), AdapterView.OnItemSelectedListen
         super.onCreate(savedInstanceState)
         viewModel.setCurrentDate(myCalendar)
     }
+    private fun differnceTrue(date1: String, date2: String?): Long {
+        var dateType1: Date
+        var dateType2: Date
+        var daydifference = "0".toLong()
+//        11/30/2021 09:40 AM
+        try {
+            var dates = SimpleDateFormat("MM/dd/yyyy")
+            dateType1 = dates.parse(date1.substringBefore(" "))
+            dateType2 = dates.parse(date2?.substringBefore(" "))
+            var differencedate = abs(dateType1.time - dateType2.time)
+            daydifference = differencedate / (24 * 60 * 60 * 1000)
+            Log.d("yash", "differnceTrue: " + daydifference)
+            return daydifference
+        } catch (e: Exception) {
+        }
+        return daydifference
+    }
 
+    fun timeCalculateForStartEndTime(hour: Int, minute: Int, isStart: String): String {
+        var timestring = ""
+        var hoursfinal: Int = 0
+        if ((hour == 12 && minute > 0) || (hour > 12 && minute > 0) || (hour > 12 && minute == 0)) {
+            if (hour == 12) {
+                hoursfinal = hour
+            } else {
+                hoursfinal = hour - 12
+            }
+            if (hoursfinal < 10) {
+                if (minute < 10) {
+                    timestring = "0$hoursfinal:0$minute PM"
+                } else {
+                    timestring = "0$hoursfinal:$minute PM"
+                }
+            } else {
+                if (minute < 10) {
+                    timestring = "$hoursfinal:0$minute PM"
+                } else {
+                    timestring = "$hoursfinal:$minute PM"
+                }
+            }
+        } else {
+            if (hour == 0) {
+                if (minute < 10) {
+                    timestring = "${hour.plus(12)}:0$minute AM"
+                } else {
+                    timestring = "${hour.plus(12)}:$minute AM"
+                }
+            } else {
+                if (hour < 10) {
+                    if (minute < 10) {
+                        timestring = "0$hour:0$minute AM"
+                    } else {
+                        timestring = "0$hour:$minute AM"
+                    }
+                } else {
+                    if (minute < 10) {
+                        timestring = "$hour:0$minute AM"
+                    } else {
+                        timestring = "$hour:$minute AM"
+                    }
+                }
+            }
+
+        }
+
+        val myFormat = "MM/dd/yyyy" //In which you need put here
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        var startDatestring = ""
+        if (isStart == "isstart") {
+            startDatestring = sdf.format(myCalendar.time)
+        } else {
+            startDatestring = sdf.format(myCalendar1.time)
+        }
+        return "$startDatestring $timestring"
+    }
     private fun startDatePickerObserver() {
         viewModel.startDateSelection.observe(requireActivity(), { event ->
             event.getContentIfNotHandled()?.let {
