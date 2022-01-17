acdc4e59800128cbfc8953ff53e9263a7af55de6
mansikothari-lnc <mansi@coppervine.io>
2022-01-17 12:31:05 +0530

    sorted the issues of scrolling the printer list individually;

--- app/src/main/java/com/android/pos/ui/fragments/settings/hardware/printer/Printer.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/hardware/printer/Printer.kt
@@ -220,6 +220,8 @@ class Printer : Fragment(), Runnable, PrinterListAdapter.PrinterListInterface,
         availableNetworkAdapter.setListner(this)
         availableNetworkAdapter.setList(arrayListOf())
         binding.rvAvailablePrinter.adapter = availableNetworkAdapter
+        binding.rvAvailablePrinter.isNestedScrollingEnabled=false
+        binding.rvAvailablePrinter.isLayoutFrozen=true
         binding.rvAvailablePrinter.addItemDecoration(
             DividerItemDecoration(
                 requireContext(),
