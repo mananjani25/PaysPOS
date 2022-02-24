9a57513557a78283b47260488c6b948b3224aac0
manan1209 <manan.jani@bacancy.com>
2022-02-24 19:29:05 +0530

    - header New dashboard screen

---
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashboardCategoryBoldPOS.kt
@@ -0,0 +1,47 @@
+package com.android.pos.ui.fragments.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.pos.databinding.FragmentDashboardCategoryBoldPosBinding
+
+class DashboardCategoryBoldPOS : Fragment() {
+    private lateinit var binding: FragmentDashboardCategoryBoldPosBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentDashboardCategoryBoldPosBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onClick()
+
+
+    }
+
+    private fun onClick() {
+        binding.layoutHeader.txtDineIn.setOnClickListener {
+
+        }
+        binding.layoutHeader.txtKeypad.setOnClickListener {
+
+        }
+        binding.layoutHeader.imgDrawer.setOnClickListener {
+
+        }
+
+        binding.layoutHeader.txtTransaction.setOnClickListener {
+
+        }
+    }
+
+}
