ac9fccb6011438d9271026f346d8af8301497431
YashSopariya13 <yash.sopariya@bacancy.com>
2022-03-02 19:39:18 +0530

    radio button for category list.

--- app/src/main/java/com/android/pos/ui/adapter/CategoriesListAdapter.kt
+++ app/src/main/java/com/android/pos/ui/adapter/CategoriesListAdapter.kt
@@ -6,12 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Filter
 import android.widget.Filterable
-import android.widget.RadioGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.android.pos.R
 import com.android.pos.data.entities.TbCategory
 import com.android.pos.databinding.ViewCategoryBinding
-import com.android.pos.generated.callback.OnCheckedChangeListener
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -27,25 +25,33 @@ class CategoriesListAdapter(private val isChoose: Boolean) :
         fun bind(item: TbCategory) {
             binding.model = item
             binding.executePendingBindings()
-            if (absoluteAdapterPosition == 0) {
+            if(absoluteAdapterPosition==0){
                 binding.firstviewCategory.visibility = View.VISIBLE
-            } else {
+            }else{
                 binding.firstviewCategory.visibility = View.GONE
             }
             if (isChoose) {
-                binding.radioChecked.isChecked = mpos == absoluteAdapterPosition
-                binding.radioChecked.visibility = View.VISIBLE
-                binding.imageCheck.visibility = View.GONE
+                if (mpos == bindingAdapterPosition) {
+                    binding.imageCheck.setImageResource(R.drawable.ic_outline_radio_button_checked)
+                } else {
+                    binding.imageCheck.setImageResource(R.drawable.ic_uncheck_circle)
+                }
             } else {
-                binding.radioChecked.visibility = View.GONE
-                binding.imageCheck.visibility = View.VISIBLE
-            }
-            binding.radioChecked.setOnClickListener {
-                mpos = absoluteAdapterPosition
-                notifyDataSetChanged()
+                binding.imageCheck.setImageResource(R.drawable.ic_baseline_menu)
             }
         }
 
+        init {
+
+
+            binding.imageCheck.setOnClickListener {
+                if (isChoose) {
+                    mpos = bindingAdapterPosition
+                    notifyDataSetChanged()
+                }
+            }
+
+        }
 
     }
 
