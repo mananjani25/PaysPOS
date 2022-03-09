32b0af4144299d7134a07a155d3a35b8b4a07ae4
YashSopariya13 <yash.sopariya@bacancy.com>
2022-03-09 16:52:14 +0530

    keypad UI added.

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
@@ -1,5 +1,6 @@
 package com.android.pos.ui.fragments.dashboard.bolddashboard
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -8,9 +9,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.activityViewModels
+import com.android.pos.R
 import com.android.pos.data.entities.TbItem
 import com.android.pos.databinding.FragmentDashboardCategoryBoldPosBinding
+
 import com.android.pos.ui.fragments.dashboard.DashBoardCategoryViewModel
+import com.android.pos.ui.fragments.manualsales.ManualSaleBoldPOS.KeyPadManualSaleFragment
 import com.android.pos.utils.callback.ItemListner
 import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
@@ -37,7 +41,6 @@ class DashboardCategoryBoldPOS : Fragment(), ItemListner {
         loadCartFragment(CartFragment())
         loadCategoryFragment(CategoryFragment(this))
 
-
     }
 
     private fun loadCategoryFragment(fragment: Fragment) {
@@ -50,11 +53,13 @@ class DashboardCategoryBoldPOS : Fragment(), ItemListner {
         fm.beginTransaction().replace(binding.frameLayoutCart.id, frag).commit()
     }
 
+    private fun loadKeyPadFragment(frag: Fragment) {
+        val fm: FragmentManager = requireActivity().supportFragmentManager
+        fm.beginTransaction().replace(binding.frameLayout.id, frag).commit()
+    }
+
     private fun onClick() {
         binding.layoutHeader.txtDineIn.setOnClickListener {
-
-        }
-        binding.layoutHeader.txtKeypad.setOnClickListener {
 
         }
         binding.layoutHeader.imgDrawer.setOnClickListener {
@@ -68,11 +73,40 @@ class DashboardCategoryBoldPOS : Fragment(), ItemListner {
         binding.layoutHeader.imgSync.setOnClickListener {
             viewModel.syncInventoryModule()
         }
+        binding.layoutHeader.txtOpenOrder.setOnClickListener {
+            loadCategoryFragment(CategoryFragment(this))
+            binding.layoutHeader.txtOpenOrder.setTextColor(resources.getColor(R.color.btnColor))
+            binding.layoutHeader.txtOpenOrder.setTypeface(
+                binding.layoutHeader.txtOpenOrder.typeface,
+                Typeface.BOLD
+            )
+            binding.layoutHeader.txtKeypad.setTextColor(resources.getColor(R.color.txtColor))
+            binding.layoutHeader.txtKeypad.setTypeface(
+                binding.layoutHeader.txtKeypad.typeface,
+                Typeface.NORMAL
+            )
+        }
+        binding.layoutHeader.txtKeypad.setOnClickListener {
+            loadKeyPadFragment(KeyPadManualSaleFragment())
+            binding.layoutHeader.txtKeypad.setTextColor(resources.getColor(R.color.btnColor))
+            binding.layoutHeader.txtKeypad.setTypeface(
+                binding.layoutHeader.txtKeypad.typeface,
+                Typeface.BOLD
+            )
+            binding.layoutHeader.txtOpenOrder.setTextColor(resources.getColor(R.color.txtColor))
+            binding.layoutHeader.txtOpenOrder.setTypeface(
+                binding.layoutHeader.txtOpenOrder.typeface,
+                Typeface.NORMAL
+            )
+
+        }
+
+
     }
 
     override fun onItemSelected(item: TbItem) {
         Log.e(TAG, "getitem:  ${Gson().toJson(item)}")
-        val fragment = AddItemFragment.newInstance(item,this)
+        val fragment = AddItemFragment.newInstance(item, this)
         loadCategoryFragment(fragment)
 
     }
