0b489f0b68bd1e8605a6218eb42c314103b88ee1
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2022-03-11 12:47:17 +0530

    - Customer > Delete bg color should be same for all & Delete font is showing blur. - Working on it

--- app/src/main/java/com/android/pos/ui/fragments/customer/Customer.kt
+++ app/src/main/java/com/android/pos/ui/fragments/customer/Customer.kt
@@ -8,6 +8,7 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -111,7 +112,7 @@ class Customer : Fragment() {
             }
 
         })
-        viewModel._customerNoDataFound.observe(viewLifecycleOwner) {event->
+        viewModel._customerNoDataFound.observe(viewLifecycleOwner) { event ->
             binding.rvEmployeeList.visibility = View.GONE
             binding.noCustomerDats.visibility = View.VISIBLE
             binding.noCustomerDats.text = event.getContentIfNotHandled()
@@ -379,7 +380,7 @@ class Customer : Fragment() {
                 underlayButtons.add(UnderlayButton(
                     "Delete",
                     0,
-                    Color.parseColor("#FFFFFF")
+                    ContextCompat.getColor(context, R.color.white_swipe)
                 ) { pos ->
                     deletedPos = pos
                     alert(
