1a873e56bf3bd9f42742afe67964cb211565a762
manan1209 <manan.jani@bacancy.com>
2022-03-14 15:20:01 +0530

    - saved work

--- app/src/main/java/com/android/pos/ui/fragments/payment/PaymentBoldPosFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/payment/PaymentBoldPosFragment.kt
@@ -5,17 +5,53 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.android.pos.databinding.FragmentPaymentBoldPosBinding
+import com.android.pos.ui.fragments.checkout.CheckoutDetailsFragmentNew
+import com.android.pos.ui.fragments.dashboard.bolddashboard.CartFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PaymentBoldPosFragment : Fragment() {
+    private lateinit var binding: FragmentPaymentBoldPosBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentPaymentBoldPosBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        loadCartFragment(CartFragment())
+        loadCategoryFragment(CheckoutDetailsFragmentNew())
+
+
     }
+    private fun loadCartFragment(frag: Fragment) {
+        val fm: FragmentManager = requireActivity().supportFragmentManager
+        val result = Bundle().apply {
+            putInt("fragmentId", binding.frameLayout.id)
+            putInt("checkoutHeaderId", binding.layoutHeaderCheckout.rlRoot.id)
+            putBoolean("isFromPayment",true)
+           // putInt("dashboardHeaderId", binding.layoutHeader.rlRoot.id)
+        }
+        frag.arguments = result
+        fm.beginTransaction().replace(binding.frameLayoutCart.id, frag).commit()
+    }
+
+    private fun loadCategoryFragment(fragment: Fragment) {
+        val fm: FragmentManager = requireActivity().supportFragmentManager
+       /* val bundle = Bundle().apply {
+            fragmentId?.let { putInt("fragmentId", it) }
+        }*/
+       // fragment.arguments = bundle
+        fm.beginTransaction().replace(binding.frameLayout.id,fragment).commit()
+       // binding.frameLayout?.let { fm.beginTransaction().replace(it, fragment).commit() }
+    }
+
 }
