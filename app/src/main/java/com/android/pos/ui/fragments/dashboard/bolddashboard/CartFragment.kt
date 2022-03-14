17e646c8b599a0bc1acfaa783b422e90e3f327d7
Zeeshan <mohd.zeeshan@bacancy.com>
2022-03-14 16:31:34 +0530

    -added new fragment as ManualSaleBoldFragment.kt -added the ui and funtionality

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -42,9 +42,13 @@ class CartFragment() : Fragment() {
         binding.lifecycleOwner = this
         Log.e("bundleData", arguments.toString())
 
-        fragmentId = arguments?.getInt("fragmentId")
-        checkoutHeaderId = arguments?.getInt("checkoutHeaderId")!!
-        dashboardHeaderId = arguments?.getInt("dashboardHeaderId")!!
+        if (arguments?.getInt("fragmentId") != null)
+            fragmentId = arguments?.getInt("fragmentId")
+        if (arguments?.getInt("checkoutHeaderId") != null)
+            checkoutHeaderId = arguments?.getInt("checkoutHeaderId")!!
+
+        if (arguments?.getInt("dashboardHeaderId") != null)
+            dashboardHeaderId = arguments?.getInt("dashboardHeaderId")!!
         return binding.root
     }
 
@@ -61,7 +65,7 @@ class CartFragment() : Fragment() {
 
         viewModel.mAllWords(
             TAKEOUT, prefProvider.getValueInt(com.android.pos.data.remote.Constants.EMPLOYEE_ID, 0)
-        ).observe(requireActivity(), {
+        ).observe(requireActivity()) {
             if (it.isNotEmpty()) {
                 Log.e(TAG, "listSize  ${Gson().toJson(it)}")
                 it[it.size - 1].items?.toCollection(arrayListOf())
@@ -80,7 +84,7 @@ class CartFragment() : Fragment() {
                 binding.tvPayNow.text = "Pay " + MethodUtils.roundOffAmount(viewModel.totalPrice)
 
             }
-        })
+        }
     }
 
     fun initListeners() {
