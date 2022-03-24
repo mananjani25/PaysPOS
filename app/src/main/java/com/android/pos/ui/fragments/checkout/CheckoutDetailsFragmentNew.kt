f9e0d685c617f70637c6311c6a5d687a6c188026
YashSopariya13 <yash.sopariya@bacancy.com>
2022-03-24 18:41:19 +0530

    bug fixed.

--- app/src/main/java/com/android/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/android/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -896,8 +896,8 @@ class CheckoutDetailsFragmentNew : Fragment(), magtekCallback,
         if (tipAmount == 0.00) {
             MethodUtils.setPriceTextView(binding.tvCash, WholetotalPrice / isSelectedCount)
             MethodUtils.setPriceTextView(binding.tvCash0, WholetotalPrice / isSelectedCount)
-            binding.tvCash.text = "Cash (" + binding.tvCash.text + ")"
             MethodUtils.setPriceTextView(binding.tvCard, WholetotalPrice / isSelectedCount)
+            binding.tvCash.text = "Cash (" + binding.tvCash.text + ")"
             binding.tvCard.text = "Card (" + binding.tvCard.text + ")"
             MethodUtils.setPriceTextView(
                 binding.tvAmount,
@@ -910,6 +910,10 @@ class CheckoutDetailsFragmentNew : Fragment(), magtekCallback,
             )
             MethodUtils.setPriceTextView(
                 binding.tvCash0,
+                (WholetotalPrice / isSelectedCount) + tipAmount
+            )
+            MethodUtils.setPriceTextView(
+                binding.tvCard,
                 (WholetotalPrice / isSelectedCount) + tipAmount
             )
             binding.tvCash.text =
