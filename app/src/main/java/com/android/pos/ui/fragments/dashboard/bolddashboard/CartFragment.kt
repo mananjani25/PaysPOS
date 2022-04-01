62ac19c244f59d7edbf1c4b2632d0ba06a90778d
Zeeshan <mohd.zeeshan@bacancy.com>
2022-04-01 17:51:10 +0530

    Manual sales > Discount is not showing in cart with updated information as it is showing proper in main cart.

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -257,7 +257,6 @@ class CartFragment(val itemClickListner: ItemClickListner?) : Fragment(), MyCall
                 tableName = dineInFloorTableModel?.tableName.toString()
                 status = dineInFloorTableModel?.status.toString()
 
-
             }
 
         }
@@ -832,6 +831,7 @@ class CartFragment(val itemClickListner: ItemClickListner?) : Fragment(), MyCall
                 )
 
 
+
             }
             negativeButton(R.string.tv_cancel) {
                 // Do negative stuff heref
@@ -928,8 +928,10 @@ class CartFragment(val itemClickListner: ItemClickListner?) : Fragment(), MyCall
                     clearCustomer()
                     viewModel.deleteCart()
                     cartlist.clear()
+                    isOrderUpdate=false
                     prefProvider.setValue(ORDER_TYPE, TAKEOUT)
                     prefProvider.setValueboolean(Constants.LOYALTY_ADDED, false)
+
                 }
             }
             negativeButton(R.string.tv_cancel) {
