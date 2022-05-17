b7669a405b79f74e3445db2d6589dcfd370fb71f
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2022-05-17 11:48:37 +0530

    - Business details auto complete search view result update logic

--- app/src/main/java/com/android/pos/data/entities/BusinessAddress.kt
+++ app/src/main/java/com/android/pos/data/entities/BusinessAddress.kt
@@ -5,12 +5,12 @@ import com.google.gson.annotations.SerializedName
 
 data class  BusinessAddress(
     @SerializedName("id") val bid: Int,
-    @SerializedName("address1") val address1: String,
-    @SerializedName("address2") val address2: String,
-    @SerializedName("city") val city: String,
-    @SerializedName("state") val state: String,
-    @SerializedName("country") val country: String,
-    @SerializedName("postcode") val postcode: String,
+    @SerializedName("address1") var address1: String,
+    @SerializedName("address2") var address2: String,
+    @SerializedName("city") var city: String,
+    @SerializedName("state") var state: String,
+    @SerializedName("country") var country: String,
+    @SerializedName("postcode") var postcode: String,
     @SerializedName("addressable_type") val addressableType: String,
     @SerializedName("addressable_id") val addressableId: Int,
     @SerializedName("created_at") val createdAt: String,
