a6fe5c3e70f1e1773727717374a503ae11ad0c2b
YashSopariya13 <yash.sopariya@bacancy.com>
2022-05-31 12:00:50 +0530

    conflict resolve.

--- app/src/main/java/com/android/pos/data/model/responseModel/VenueDetailsResponse.kt
+++ app/src/main/java/com/android/pos/data/model/responseModel/VenueDetailsResponse.kt
@@ -94,8 +94,7 @@ data class VenueDetailsResponse(
         @SerializedName("service_charge_enable")
         val service_charge_enable: Boolean,
         @SerializedName("enable_dine_in_service_charge")
-        val enable_dine_in_service_charge: Boolean
-        val magensaSettings: List<MagensaSettings>,
+        val enable_dine_in_service_charge: Boolean,
         @SerializedName("shift_report_configuration")
         val shift_report_configuration: ShiftRportConfiguration?,
         @SerializedName("time_zone_options")
