218eca73c8802f28790146730c50bd441588e382
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2022-06-08 11:54:37 +0530

    - url change

--- app/src/main/java/com/android/pos/di/ApiModule.kt
+++ app/src/main/java/com/android/pos/di/ApiModule.kt
@@ -22,8 +22,8 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object ApiModule {
 
-    // private const val BASE_URL = "https://possoft.io/api/v1/"
-    public const val BASE_URL = "https://hugepos.com/api/v1/"  // for BOLD POS
+     private const val BASE_URL = "https://possoft.io/api/v1/"
+//    public const val BASE_URL = "https://hugepos.com/api/v1/"  // for BOLD POS
     //  private const val BASE_URL = "http://34.205.43.53/api/v1/"
     //private const val BASE_URL = "https://possoft.io/api/v1/"
 
