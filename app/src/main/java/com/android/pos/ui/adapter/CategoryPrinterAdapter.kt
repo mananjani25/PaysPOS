275c81c74beec623a56bd7c58b3ab9f8d032f965
manan1209 <manan.jani@bacancy.com>
2022-07-07 13:57:50 +0530

    - saved commit

---
+++ app/src/main/java/com/android/pos/ui/adapter/CategoryPrinterAdapter.kt
@@ -0,0 +1,22 @@
+package com.android.pos.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryPrinterAdapter() : RecyclerView.Adapter<CategoryPrinterAdapter.MyViewHolder>() {
+    //inner class MyViewHolder(val itemView:)
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CategoryPrinterAdapter.MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: CategoryPrinterAdapter.MyViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
