dbbdd5c8758c639b8f50ad9d9e0b4bcbc79a1139
manan1209 <manan.jani@bacancy.com>
2022-07-22 12:15:50 +0530

    - working on sunmi Drawer

--- app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -2138,7 +2138,9 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
 
                 try {
-                    builder.addPulse(Printer.DRAWER_HIGH, Printer.PULSE_100)
+                    if (paymentType.equals("Cash", true)) {
+                        builder.addPulse(Printer.DRAWER_HIGH, Printer.PULSE_100)
+                    }
                     PrinterClass.getPrinter()?.sendData(
                         builder,
                         PrinterClass.BLUETOOTH_TIMEOUT, status, battery
@@ -4181,8 +4183,13 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
                 try {
 
-
-                    builder.addPulse(Printer.DRAWER_HIGH, Printer.PULSE_100)
+                    if (receiptModel?.order?.payments?.get(receiptModel?.order?.payments?.size!! - 1)?.paymentType.equals(
+                            "Cash",
+                            true
+                        )
+                    ) {
+                        builder.addPulse(Printer.DRAWER_HIGH, Printer.PULSE_100)
+                    }
 
                     PrinterClass.getPrinter()?.sendData(
                         builder,
@@ -6964,10 +6971,16 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
 
             try {
-                builder.addPulse(
-                    com.epson.epos2.printer.Printer.DRAWER_HIGH,
-                    com.epson.epos2.printer.Printer.PULSE_100
-                )
+                if (receiptModel?.order?.payments?.get(receiptModel?.order?.payments?.size!! - 1)?.paymentType.equals(
+                        "Cash",
+                        true
+                    )
+                ) {
+                    builder.addPulse(
+                        com.epson.epos2.printer.Printer.DRAWER_HIGH,
+                        com.epson.epos2.printer.Printer.PULSE_100
+                    )
+                }
                 PrinterClass.getPrinter()?.sendData(
                     builder,
                     BLUETOOTH_TIMEOUT, status, battery
