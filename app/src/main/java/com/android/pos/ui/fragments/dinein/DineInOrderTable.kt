351e6f69532b338bf13f4988174f7041f03eb20c
manan1209 <manan.jani@bacancy.com>
2022-07-25 16:16:39 +0530

    - dine in update order items null fixed

--- app/src/main/java/com/android/pos/ui/fragments/dinein/DineInOrderTable.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dinein/DineInOrderTable.kt
@@ -673,6 +673,24 @@ class DineInOrderTable : Fragment(), DineInTableAdapter.DineInTableListner {
                     model.isFired = list[i].isFired
                     model.guestDividerAmt = list[i].guestDividerAmt
                     model.guestDividedAmt = list[i].guestDividedAmt
+
+
+                    for (j in i + 1 until list.size) {
+                        if (list[j].isHeader == 1) {
+                            list[j].item?.let { it1 ->
+                                if (it1.discountPrice != 0.0) {
+                                    it1.discountPrice =
+                                        MethodUtils.roundOffAmountDouble(it1.discountPrice / it1.itemQuantity)
+                                }
+                                Log.e(TAG, "updateItemForDiscount  ${Gson().toJson(it1)}")
+                                listTbItem.add(it1)
+                            }
+
+                        } else {
+                            break
+                        }
+
+                    }
                     model.items = listTbItem
                     newList.add(model)
 
