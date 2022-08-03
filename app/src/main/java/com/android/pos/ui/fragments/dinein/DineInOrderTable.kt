3d46107bfb921c47bb23c042c8e15ef3a57b59dd
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2022-08-03 16:26:58 +0530

    - null check issue

--- app/src/main/java/com/android/pos/ui/fragments/dinein/DineInOrderTable.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dinein/DineInOrderTable.kt
@@ -1098,6 +1098,7 @@ class DineInOrderTable : Fragment(), DineInTableAdapter.DineInTableListner {
 
         viewModel.msgText.observe(viewLifecycleOwner) { event ->
             event.getContentIfNotHandled()?.let {
+                Log.e("AlertUtils", it.toString())
                 if (it.toString() != "null") {
                     AlertUtils.showCustomAlert(requireContext(), it)
                 }
@@ -1109,6 +1110,7 @@ class DineInOrderTable : Fragment(), DineInTableAdapter.DineInTableListner {
     }
 
     private fun setupSnackbar() {
+        Log.e("AlertUtils", viewModel.snackbarText.value.toString())
         if (viewModel.snackbarText.value != null)
             binding.root.liveSnackBar(this, viewModel.snackbarText, Snackbar.LENGTH_SHORT)
 
@@ -7949,6 +7951,7 @@ class DineInOrderTable : Fragment(), DineInTableAdapter.DineInTableListner {
         viewModel.unMergeStatusUpdate.observe(viewLifecycleOwner) { event ->
             event.getContentIfNotHandled()?.let { status ->
 
+                Log.e("AlertUtils 1",status)
                 AlertUtils.showCustomAlertWithListenerWithOK(
                     requireContext(), status.toString()
                 ) { _, _ ->
