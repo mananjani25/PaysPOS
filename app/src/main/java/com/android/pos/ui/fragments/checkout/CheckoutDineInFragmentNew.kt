d26e4c8cfdd0646206577f76d170b792f502a972
YashSopariya13 <yash.sopariya@bacancy.com>
2022-08-24 13:03:42 +0530

    cursor move in next field in checkout payment screen manual card.

--- app/src/main/java/com/android/pos/ui/fragments/checkout/CheckoutDineInFragmentNew.kt
+++ app/src/main/java/com/android/pos/ui/fragments/checkout/CheckoutDineInFragmentNew.kt
@@ -3,7 +3,9 @@ package com.android.pos.ui.fragments.checkout
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -195,8 +197,66 @@ class CheckoutDineInFragmentNew(val dineInDataModel: CheckOutDineInDataModel) : 
         observeShowProgress()
         observeData()
         callback()
+        setUpManualCardFocusChanged()
     }
+    private fun setUpManualCardFocusChanged() {
+        binding.edtCardNumber.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    if(s?.length==22){
+                        binding.edtMMYY.requestFocus()
+                    }
+                } catch (e: Exception) {
+                }
+            }
+        })
+        binding.edtMMYY.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    if(s?.length==5){
+                        binding.edtCVV.requestFocus()
+                    }else if(s?.length==0){
+                        binding.edtCardNumber.requestFocus()
+                    }
+                } catch (e: Exception) {
+                }
+            }
+        })
+        binding.edtCVV.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                try {
+                    if(s?.length==0){
+                        binding.edtMMYY.requestFocus()
+                    }
+                } catch (e: Exception) {
+                }
+            }
+        })
+    }
     @SuppressLint("SetTextI18n")
     private fun callback() {
         requireActivity().supportFragmentManager.setFragmentResultListener(
