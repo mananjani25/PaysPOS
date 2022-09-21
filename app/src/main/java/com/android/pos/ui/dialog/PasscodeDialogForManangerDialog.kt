cdfde477534d8179b7e685ef7086c14ce4b2376d
YashSopariya13 <yash.sopariya@bacancy.com>
2022-09-21 11:57:00 +0530

    working on passcode manager.

--- app/src/main/java/com/android/pos/ui/dialog/PasscodeDialogForManangerDialog.kt
+++ app/src/main/java/com/android/pos/ui/dialog/PasscodeDialogForManangerDialog.kt
@@ -3,29 +3,30 @@ package com.android.pos.ui.dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.InsetDrawable
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import com.android.pos.R
+import com.android.pos.databinding.PasscodeDialogForManangerFragmentBinding
 
-class PasscodeDialogForMananger : DialogFragment() {
+class PasscodeDialogForManangerDialog : DialogFragment() {
 
+    lateinit var binding:PasscodeDialogForManangerFragmentBinding
     companion object {
-        fun newInstance() = PasscodeDialogForMananger()
+        fun newInstance() = PasscodeDialogForManangerDialog()
     }
-
-    private lateinit var viewModel: PasscodeDialogForManangerViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.passcode_dialog_for_mananger_fragment, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.passcode_dialog_for_mananger_fragment, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -1657,19 +1657,25 @@ class CartFragment(
                         )
                     }
                     R.id.menu_discount -> {
-
-                        val bundle = Bundle()
-                        bundle.putBoolean("isOrderDiscount", true)
-                        bundle.putDouble("totalPrice", viewModel.subTotalPrice)
-                        if (cartlist.isNotEmpty()) {
-                            bundle.putDouble("orderDiscountPrice", cartlist[0].discountPrice)
-                            bundle.putString("orderDiscountType", cartlist[0].discountType)
-                            bundle.putDouble("selectedvalue", cartlist[0].discountSelectdValue)
+                        if (prefProvider.isAdmin()){
+                            val bundle = Bundle()
+                            bundle.putBoolean("isOrderDiscount", true)
+                            bundle.putDouble("totalPrice", viewModel.subTotalPrice)
+                            if (cartlist.isNotEmpty()) {
+                                bundle.putDouble("orderDiscountPrice", cartlist[0].discountPrice)
+                                bundle.putString("orderDiscountType", cartlist[0].discountType)
+                                bundle.putDouble("selectedvalue", cartlist[0].discountSelectdValue)
+                            }
+                            findNavController().navigate(
+                                R.id.action_dashboardCategoryBoldPOS_to_addDiscountDialog,
+                                bundle
+                            )
+                        }else{
+                            findNavController().navigate(
+                                R.id.actionboldpos_to_pascodeManagerDailog
+                            )
                         }
-                        findNavController().navigate(
-                            R.id.action_dashboardCategoryBoldPOS_to_addDiscountDialog,
-                            bundle
-                        )
+
 
                     }
                     /*R.id.menu_note -> {
