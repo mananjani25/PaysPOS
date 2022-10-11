ce77a46b8c33466fdfd6ef0fcc87afc49e6eb2b9
YashSopariya13 <yash.sopariya@bacancy.com>
2022-10-11 14:59:30 +0530

    modifier issue fixed.

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
@@ -3300,6 +3300,17 @@ class DashBoardCategoryViewModel @Inject constructor(
 
 
                                 category.items.forEach {
+                                    it.modifierSets.forEach { modifierset ->
+                                        val itemModifierSets = ItemModifierSets().apply {
+                                            itemId = it.id
+                                            modifierSetId = modifierset.id!!
+                                            minRequired = modifierset.min_required
+                                            maxAllowed = modifierset.max_allowed
+                                            isDeleted = modifierset.isDeleted
+                                        }
+                                        itemModifierSetList.add(itemModifierSets)
+                                    }
+
                                     //new optimise code
                                     inventoryModelList.add(TbItem().convertToItem(it, category))
                                 }
@@ -3308,18 +3319,18 @@ class DashBoardCategoryViewModel @Inject constructor(
                             }
 
 
-                            mData.modifierSets.forEach { modifierSets ->
-
-                                val itemModifierSets = ItemModifierSets().apply {
-                                    itemId = this.modifierSetId
-                                    modifierSetId = modifierSets.id!!
-                                    minRequired = modifierSets.min_required
-                                    maxAllowed = modifierSets.max_allowed
-                                    isDeleted = modifierSets.isDeleted
-                                }
-
-                                itemModifierSetList.add(itemModifierSets)
-                            }
+//                            mData.modifierSets.forEach { modifierSets ->
+//
+//                                val itemModifierSets = ItemModifierSets().apply {
+//                                    itemId = this.modifierSetId
+//                                    modifierSetId = modifierSets.id!!
+//                                    minRequired = modifierSets.min_required
+//                                    maxAllowed = modifierSets.max_allowed
+//                                    isDeleted = modifierSets.isDeleted
+//                                }
+//
+//                                itemModifierSetList.add(itemModifierSets)
+//                            }
                             modifierSetList.addAll(mData.modifierSets)
 
 
