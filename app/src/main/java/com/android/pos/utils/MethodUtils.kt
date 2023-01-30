49ea9e1bed83efbcab90ed5a7b5cbe99cad018e2
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2023-01-30 15:51:11 +0530

    markup modifier and variation price issue fixed

--- app/src/main/java/com/android/pos/utils/MethodUtils.kt
+++ app/src/main/java/com/android/pos/utils/MethodUtils.kt
@@ -222,7 +222,7 @@ class MethodUtils {
             createItemRequestMap["location_id"] =
                 data.locationId.toString().toMultiPartRequestBody()
             createItemRequestMap["name"] = data.name.toString().toMultiPartRequestBody()
-            createItemRequestMap["price"] = data.price.toString().toMultiPartRequestBody()
+            createItemRequestMap["price_without_markup"] = data.price.toString().toMultiPartRequestBody()
             createItemRequestMap["priceType"] = data.priceType.toString().toMultiPartRequestBody()
             createItemRequestMap["product_code"] =
                 data.productCode.toString().toMultiPartRequestBody()
