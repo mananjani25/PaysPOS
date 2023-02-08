c5bcda6e0ac56d0223c8e5f281d0ab133be87dd9
Bacancy-Vishal <vishal.j.patel@bacancy.com>
2023-02-08 23:16:47 +0530

    Menu changes Slogan Change 2022 to 2023 Change Menu to Settings

--- app/src/main/java/com/android/pos/ui/fragments/posmenu/MenuFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/posmenu/MenuFragment.kt
@@ -48,7 +48,7 @@ class MenuFragment : DialogFragment() {
     }
 
     private fun setUpHeader() {
-        binding.header.txtTitle.text = getString(R.string.menu)
+        binding.header.txtTitle.text = "Settings"
         binding.header.txtSave.text = getString(R.string.tv_home)
     }
 
