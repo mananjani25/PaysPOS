28d7f05606f842f6757916272c89e549c75f67aa
manan1209 <manan.jani@bacancy.com>
2023-02-17 14:52:33 +0530

    - saved commit

--- app/src/main/java/com/android/pos/ui/adapter/boldpos/CategoryAdapter.kt
+++ app/src/main/java/com/android/pos/ui/adapter/boldpos/CategoryAdapter.kt
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.android.pos.data.model.CategoryTabModel
 import com.android.pos.databinding.ViewBoldCategoryBinding
 import com.android.pos.ui.adapter.CategoryTabAdapter1
-import com.android.pos.utils.MethodUtils
 
 class CategoryAdapter(
     val context: Context,
@@ -38,10 +37,10 @@ class CategoryAdapter(
                 binding.txtCategoryName.text = model.title
             }
             binding.root.setOnClickListener {
-                if (MethodUtils.isDoubleClickCategory()) return@setOnClickListener
+                //if (MethodUtils.isDoubleClickCategory()) return@setOnClickListener
 
 
-                listner.onTabSelected(bindingAdapterPosition)
+              //  listner.onTabSelected(bindingAdapterPosition)
                 list.forEachIndexed { index, categoryTabModel ->
 
                     categoryTabModel.isSelected = index == bindingAdapterPosition
