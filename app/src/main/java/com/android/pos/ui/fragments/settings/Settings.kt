b7af4f523b3f29cd0b9686d42bc83bf321a62f83
Rajeshree Savaliya <rajeshree.savaliya@bacancy.com>
2023-03-13 10:28:56 +0530

    - BIS-323: changed Home button color in dine in - BIS-307: Open order > cancel order reason > removed play button icon - BIS-276: set same height for all buttons in complete order screen - BIS-356: fixed Setup module > screen flicker issue - BIS-357: fixed Menu module > all submodule flicker issue

--- app/src/main/java/com/android/pos/ui/fragments/settings/Settings.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/Settings.kt
@@ -236,7 +236,9 @@ class Settings : Fragment() {
             binding.txtEmployee.styleNormal()
             binding.txtReports.styleNormal()
 
-            setAdapter(0)
+            if(binding.rvBusiness.visibility == View.GONE) {
+                setAdapter(0)
+            }
 
         }
         binding.txtHardware.setOnClickListener {
@@ -352,7 +354,9 @@ class Settings : Fragment() {
         binding.rvBusiness.adapter = BusinessSettingAdapter(requireContext(), list, object :
             BusinessSettingAdapter.BusinessListInterface {
             override fun onClick(pos: Int) {
-                setupView(pos)
+                if (!list[pos].isSelected) {
+                    setupView(pos)
+                }
 
             }
 
@@ -418,7 +422,7 @@ class Settings : Fragment() {
 
     private fun loadFragment(frag: Fragment) {
         val fm: FragmentManager = requireActivity().supportFragmentManager
-        fm.beginTransaction().replace(binding.frameLayout.id, frag).commit()
+            fm.beginTransaction().replace(binding.frameLayout.id, frag).commit()
 
     }
 
