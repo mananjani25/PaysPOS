40e768c12cba2a697def386be6650df81d6297a0
manan1209 <manan.jani@bacancy.com>
2023-03-20 14:27:53 +0530

    - merge branch POSFRONT-2159

--- app/src/main/java/com/android/pos/di/ApiModule.kt
+++ app/src/main/java/com/android/pos/di/ApiModule.kt
@@ -6,7 +6,6 @@ import com.android.pos.data.remote.ApiService
 import com.android.pos.data.remote.Constants.AUTH_TOKEN
 import com.android.pos.data.remote.Constants.BASE_URL_NEW
 import com.android.pos.data.remote.NetworkConnectionInterceptor
-import com.localebro.okhttpprofiler.OkHttpProfilerInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
