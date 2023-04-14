eab3533a1e1683d0ad0a9caaec3478f7dcebd1c2
Rajeshree Savaliya <rajeshree.savaliya@bacancy.com>
2023-04-14 10:11:03 +0530

    SPRINT_10_14_APR_R : BIS-522:Fixed > Addition occurs of two different order discount

--- app/src/main/java/com/android/pos/ui/fragments/manualsales/ManualSaleNew.kt
+++ app/src/main/java/com/android/pos/ui/fragments/manualsales/ManualSaleNew.kt
@@ -584,7 +584,7 @@ class ManualSaleNew : Fragment(), ManualSaleCartAdapter.ManualSaleInterface,
                     val mergeItems = merge(mainItems!!, manualItems!!)
                     mainCartList[0].items = mergeItems
                     if (cartList!![0].discountPrice != 0.00)
-                        mainCartList[0].discountPrice += cartList!![0].discountPrice
+                        mainCartList[0].discountPrice = cartList!![0].discountPrice
 
                     if (cartList!![0].note.isNotEmpty())
                         mainCartList[0].note = cartList!![0].note
