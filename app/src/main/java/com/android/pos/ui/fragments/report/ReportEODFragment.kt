b31fb1a9771e56c3172a69c9487ff10b5b1d9dfa
Dharmesh Basapati <dharmesh.basapati@bacancy.com>
2023-07-05 16:22:07 +0530

    Worked on updating validation for max days selection from 7 to 30 in EOD Report screen.

--- app/src/main/java/com/android/pos/ui/fragments/report/ReportEODFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/report/ReportEODFragment.kt
@@ -2636,12 +2636,12 @@ class ReportEODFragment : Fragment(), AdapterView.OnItemSelectedListener {
             timecalender.set(Calendar.HOUR_OF_DAY, hour)
             timecalender.set(Calendar.MINUTE, minute)
             viewModel.startDate.value = timeCalculateForStartEndTime(hour, minute, "isstart")
-            if (differnceTrue(viewModel.startDate.value!!, viewModel.endDate.value) <= 7) {
+            if (differnceTrue(viewModel.startDate.value!!, viewModel.endDate.value) <= 30) {
                 viewModel.getReportSummary("")
             } else {
                 AlertUtils.showCustomAlertWithListenerWithOK(
                     requireActivity(),
-                    "Please Select date in 7 Days."
+                    "Please Select date in 30 Days."
                 ) { _, _ ->
                 }
             }
@@ -2858,52 +2858,17 @@ class ReportEODFragment : Fragment(), AdapterView.OnItemSelectedListener {
                 val uiScope = CoroutineScope(Dispatchers.Main)
                 uiScope.launch {
 
-                    Log.d(TAG, "INSIDE JOB: CALLED")
+                    Log.d(TAG, "BEFORE JOB: CALLED")
                     ProgressUtils.showProgressDialog(requireActivity())
-
-                    delay(3000)
 
                     val job = async(Dispatchers.Main) {
                         updateData(it)
                     }
                     job.await()
 
-                    Log.d(TAG, "AFTER JOB ENDS: CALLED")
+                    Log.d(TAG, "AFTER JOB: CALLED")
                     ProgressUtils.dismissProgressDialog()
-
                 }
-
-//                uiScope.executeAsyncTask(
-//                    onPostExecute = {
-//                        ProgressUtils.dismissProgressDialog()
-//                    },
-//                    onPreExecute = {
-//                        ProgressUtils.showProgressDialog(requireActivity())
-//                    },
-//                    doInBackground = {
-//                        updateData(it)
-//                    }
-//                )
-
-//                val executor: ExecutorService = Executors.newSingleThreadExecutor()
-//                val handler = Handler(Looper.getMainLooper())
-//                executor.execute {
-//
-//                    handler.post {
-//                        //UI Thread work here
-//                        ProgressUtils.showProgressDialog(requireActivity())
-//                    }
-//
-//                    //Background work here
-//                    runOnUiThread(){
-//                        updateData(it)
-//                    }
-//
-//                    handler.post {
-//                        //UI Thread work here
-//                        ProgressUtils.dismissProgressDialog()
-//                    }
-//                }
 
             }
         })
