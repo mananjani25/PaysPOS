f6bad8ab65a7387ffb9c492540d21eb99960d920
Dharmesh Basapati <dharmesh.basapati@bacancy.com>
2023-07-20 09:25:24 +0530

    Updated CustomerAddressTextWatcher.

--- app/src/main/java/com/android/pos/utils/CustomerAddressTextWatcher.kt
+++ app/src/main/java/com/android/pos/utils/CustomerAddressTextWatcher.kt
@@ -3,11 +3,13 @@ package com.android.pos.utils
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.widget.CheckBox
 import android.widget.EditText
 
 import com.android.pos.utils.callback.AddressTextChangeListner
 
 class CustomerAddressTextWatcher(
+    var checkBox: CheckBox,
     var edittext: EditText,
     var changeField: Boolean,
     var listner: AddressTextChangeListner,
