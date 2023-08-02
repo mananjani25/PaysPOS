0ed911e973bdd52958f4936719241a836cf02176
manan1209 <manan.jani@bacancy.com>
2023-08-02 13:12:25 +0530

    Bug Fixed.  - BIS-1120 when update open order new added item was not printing and handle direct pay scenario for same.

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
@@ -417,7 +417,6 @@ class DashBoardCategoryViewModel @Inject constructor(
 
     fun deleteCart() {
         prefProvider.setValueInt(Constants.CAT_ID_SELECTED, 0)
-        //prefProvider.setValue(Constants.OPEN_ORDER_ITEMS,"")
         cartModel = null
         GlobalScope.launch {
             posRepository.deleteCart(prefProvider.getValueInt(EMPLOYEE_ID, 0))
