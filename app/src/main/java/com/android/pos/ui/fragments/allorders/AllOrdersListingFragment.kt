203c7bb8e07e198cb919ba25895918fee2572f7a
Rajeshree Savaliya <rajeshree.savaliya@bacancy.com>
2023-08-09 17:36:13 +0530

    BIS-1133 : Added missing delivery type printing for all type of printers in kitchen receipt

--- app/src/main/java/com/android/pos/ui/fragments/allorders/AllOrdersListingFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/allorders/AllOrdersListingFragment.kt
@@ -3866,11 +3866,28 @@ class AllOrdersListingFragment(
                     )
                     builder.addTextAlign(Builder.ALIGN_CENTER)
 
-                    addBuilderTextForU220(builder, "Online Order")
+                    addBuilderTextForU220(builder, orderData.orderTypeName.toString())
+
+                    if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                                orderData.orderType.equals("OnlineWebOrder", true) ||
+                                orderData.orderType.equals("Online Order", true) ||
+                                orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                        builder.addFeedLine(1)
+                        builder.addTextFont(Builder.FONT_E)
+                        builder.addTextLang(Builder.LANG_EN)
+                        builder.addTextSize(fontSizeH, fontSizeW)
+                        builder.addTextStyle(
+                            Builder.FALSE,
+                            Builder.FALSE,
+                            Builder.TRUE,
+                            Builder.COLOR_1
+                        )
+                        builder.addTextAlign(Builder.ALIGN_CENTER)
+
+                        addBuilderTextForU220(builder, orderData.deliveryType.toString())
+                        builder.addFeedLine(1)
+                    }
                 }
-                var tmps = "Open Order".toString().trim()
-                    .toString().lowercase()
-                LogUtil.logE(TAG, "LowerCAse ${tmps.trimmedLength()}")
 
 
                 if (kitchenSettingModel.showTeamMember) {
@@ -4141,6 +4158,27 @@ class AllOrdersListingFragment(
                     builder.addTextAlign(Builder.ALIGN_CENTER)
 
                     addBuilderTextForU220(builder, "Online Order")
+                    addBuilderTextForU220(builder, orderData.orderTypeName.toString())
+
+                    if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                                orderData.orderType.equals("OnlineWebOrder", true) ||
+                                orderData.orderType.equals("Online Order", true) ||
+                                orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                        builder.addFeedLine(1)
+                        builder.addTextFont(Builder.FONT_E)
+                        builder.addTextLang(Builder.LANG_EN)
+                        builder.addTextSize(fontSizeH, fontSizeW)
+                        builder.addTextStyle(
+                            Builder.FALSE,
+                            Builder.FALSE,
+                            Builder.TRUE,
+                            Builder.COLOR_1
+                        )
+                        builder.addTextAlign(Builder.ALIGN_CENTER)
+
+                        addBuilderTextForU220(builder, orderData.deliveryType.toString())
+                        builder.addFeedLine(1)
+                    }
                 }
 
 
@@ -4494,12 +4532,28 @@ class AllOrdersListingFragment(
                     )
                     builder.addTextAlign(Builder.ALIGN_CENTER)
 
-                    addBuilderText(builder, "Online Order")
-                }
-                var tmps = "Open Order".toString().trim()
-                    .toString().lowercase()
-                LogUtil.logE(TAG, "LowerCAse ${tmps.trimmedLength()}")
+                    addBuilderText(builder, orderData.orderTypeName.toString())
 
+                    if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                                orderData.orderType.equals("OnlineWebOrder", true) ||
+                                orderData.orderType.equals("Online Order", true) ||
+                                orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                        builder.addFeedLine(1)
+                        builder.addTextFont(Builder.FONT_E)
+                        builder.addTextLang(Builder.LANG_EN)
+                        builder.addTextSize(fontSizeH, fontSizeW)
+                        builder.addTextStyle(
+                            Builder.FALSE,
+                            Builder.FALSE,
+                            Builder.TRUE,
+                            Builder.COLOR_1
+                        )
+                        builder.addTextAlign(Builder.ALIGN_CENTER)
+
+                        addBuilderText(builder, orderData.deliveryType.toString())
+                        builder.addFeedLine(1)
+                    }
+                }
 
                 if (kitchenSettingModel.showTeamMember) {
 
@@ -4777,7 +4831,27 @@ class AllOrdersListingFragment(
                     )
                     builder.addTextAlign(Builder.ALIGN_CENTER)
 
-                    addBuilderText(builder, "Online Order")
+                    addBuilderText(builder, orderData.orderTypeName.toString())
+
+                    if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                                orderData.orderType.equals("OnlineWebOrder", true) ||
+                                orderData.orderType.equals("Online Order", true) ||
+                                orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                        builder.addFeedLine(1)
+                        builder.addTextFont(Builder.FONT_E)
+                        builder.addTextLang(Builder.LANG_EN)
+                        builder.addTextSize(fontSizeH, fontSizeW)
+                        builder.addTextStyle(
+                            Builder.FALSE,
+                            Builder.FALSE,
+                            Builder.TRUE,
+                            Builder.COLOR_1
+                        )
+                        builder.addTextAlign(Builder.ALIGN_CENTER)
+
+                        addBuilderText(builder, orderData.deliveryType.toString())
+                        builder.addFeedLine(1)
+                    }
                 }
 
 
@@ -5077,9 +5151,17 @@ class AllOrdersListingFragment(
             SunmiPrinterApi.getInstance().lineWrap(1)
 
             if (kitchenSettingModel.showOrderType) {
-                PrintSunmiUtils.printOrderType("Online Order")
+                PrintSunmiUtils.printOrderType(orderData.orderTypeName.toString())
+                SunmiPrinterApi.getInstance().lineWrap(1)
+
+                if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                    orderData.orderType.equals("OnlineWebOrder", true) ||
+                    orderData.orderType.equals("Online Order", true) ||
+                    orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                    PrintSunmiUtils.printOrderType(orderData.deliveryType.toString())
+                    SunmiPrinterApi.getInstance().lineWrap(1)
+                }
             }
-            SunmiPrinterApi.getInstance().lineWrap(1)
 
             if (kitchenSettingModel.showTeamMember) {
 
@@ -5891,7 +5973,16 @@ class AllOrdersListingFragment(
 
 
             if (kitchenSettingModel.showOrderType) {
-                PrintSunmiUtils.headerText("Online Order")
+                PrintSunmiUtils.headerText(orderData.orderTypeName.toString())
+                SunmiPrinterApi.getInstance().lineWrap(1)
+
+                if ((orderData.orderType.equals(Constants.PHONE_ORDER, true) ||
+                            orderData.orderType.equals("OnlineWebOrder", true) ||
+                            orderData.orderType.equals("Online Order", true) ||
+                            orderData.orderType.equals("OnlineOrder", true)) && orderData.deliveryType != null) {
+                    PrintSunmiUtils.headerText(orderData.deliveryType.toString())
+                    SunmiPrinterApi.getInstance().lineWrap(1)
+                }
             }
 
             if (kitchenSettingModel.showTeamMember) {
