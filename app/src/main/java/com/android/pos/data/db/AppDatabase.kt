4528597e28cae00b23e29f0f29ed80ae3346a788
Dharmesh Basapati Bacancy <dharmesh.basapati@bacancy.com>
2023-09-05 18:14:15 +0530

    Resolved migration issue in app database.

--- app/src/main/java/com/android/pos/data/db/AppDatabase.kt
+++ app/src/main/java/com/android/pos/data/db/AppDatabase.kt
@@ -107,7 +107,7 @@ import com.android.pos.data.typeconvert.TypeConvertorPhone
         GetCustomerReceiptSettingsResponse.Data::class, LoyaltyProgramsModel::class, SplitDetailListModel::class,
         CashDiscountModel::class, TbCountryList::class, TbCardReader::class, VenueDetailsResponse.Data.CancelOrderReason::class,
         DineInCartModel::class, ShiftRportConfiguration::class, TbBusinessDetails::class, TbTimeZones::class, PrinterQueueModel::class, VenueDetailsResponse.Data.WastageReason::class],
-    version = 7
+    version = 8
 )
 @TypeConverters(
     TypeConvertersIds::class,
@@ -264,6 +264,7 @@ abstract class AppDatabase : RoomDatabase() {
         private fun buildDatabase(appContext: Context) =
             Room.databaseBuilder(appContext, AppDatabase::class.java, DATABASE_NAME)
                 .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7)
+                .fallbackToDestructiveMigration()
                 .build()
     }
 
--- app/src/main/java/com/android/pos/data/model/GuestAttrQueue.kt
+++ app/src/main/java/com/android/pos/data/model/GuestAttrQueue.kt
@@ -2,8 +2,11 @@ package com.android.pos.data.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.android.pos.data.entities.TypeConvertersQueueDineIn
 import com.android.pos.data.model.responseModel.CreateOrderResponse
 
+@TypeConverters(TypeConvertersQueueDineIn::class)
 @Entity
 data class GuestAttrQueue(
     @PrimaryKey(autoGenerate = true)
