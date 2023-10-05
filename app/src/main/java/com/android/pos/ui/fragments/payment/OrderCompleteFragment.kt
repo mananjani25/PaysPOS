46f81af045f930d63b94d73840ddcb58981e9a2e
admin <suresh.prajapati@bacancy.com>
2023-10-05 15:29:30 +0530

    https://snack.atlassian.net/browse/BIS-3271
    Printer queue disabled> Save order > Click on update and add one item and on direct payment two receipts are fired from sunmi external printers.
    
    pushed by suresh
    05/10/2023

--- app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/android/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -6096,7 +6096,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                 Status.SUCCESS -> {
                     ProgressUtils.dismissProgressDialog()
                     if (it.data != null && isPrint == true) {
-
+                        isPrint = false
 
                         kitchenPrinterList = it.data
 
