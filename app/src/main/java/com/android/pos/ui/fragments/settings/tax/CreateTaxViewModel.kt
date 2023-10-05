a4dce2f100d8fe5633f26c9e369bc2fc6cdda937
Rajeshree Savaliya <rajeshree.savaliya@bacancy.com>
2023-10-05 12:50:06 +0530

    BIS-2667 : Fixed tax data update issue

--- app/src/main/java/com/android/pos/ui/fragments/settings/tax/CreateTaxViewModel.kt
+++ app/src/main/java/com/android/pos/ui/fragments/settings/tax/CreateTaxViewModel.kt
@@ -99,6 +99,7 @@ class CreateTaxViewModel @Inject constructor(
                         if (item != null) {
                             tempTaxData = item.taxes as ArrayList<TaxData> ?: arrayListOf()
                             if (!tempTaxData.contains(element = tax)) {
+                                tempTaxData.removeIf { taxData-> taxData.id == tax.id }
                                 tempTaxData.add(tax)
                             }
                             posRepository.updateTaxDataForItem(tempTaxData, it)
