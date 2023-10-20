ed07e82ebdbf7e9f7f95a6ff098b46672b124094
admin <suresh.prajapati@bacancy.com>
2023-10-20 13:24:55 +0530

    https://snack.atlassian.net/browse/BIS-3401
    Printer queue enabled >Pick up option by default print on kitchen receipt after saving the (takeout and open order)
    
    code updated for 3.28 build.
    
    20/10/2023

--- app/src/main/java/com/android/pos/utils/workmanager/UploadWorker2.kt
+++ app/src/main/java/com/android/pos/utils/workmanager/UploadWorker2.kt
@@ -306,6 +306,8 @@ class UploadWorker2(@NotNull context: Context, @NotNull params: WorkerParameters
 
                             try {
                                 if (it.asJsonObject.has("delivery_type")){
+                                    if (it.asJsonObject.get("order_type").asString == Constants.PHONE_ORDER.lowercase() ||
+                                        it.asJsonObject.get("order_type").asString == Constants.OPEN_ORDER.lowercase())
                                     modelOrder.deliveryType = it.asJsonObject.get("delivery_type").asString
                                 }
                             }catch (e:Exception){
