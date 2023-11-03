6d6cf196408a654923f34b128016ec4f4cb19156
Rajeshree Savaliya <rajeshree.savaliya@bacancy.com>
2023-11-03 16:48:57 +0530

    - commented calculation logic

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/DashBoardCategoryViewModel.kt
@@ -391,6 +391,7 @@ class DashBoardCategoryViewModel @Inject constructor(
 
     }
 
+    // combine two similar items
     fun generateCombinedItems(cartModel: CartModel): CartModel {
         val combinedItems = arrayListOf<TbItem>()
         cartModel.items?.let {
@@ -581,7 +582,7 @@ class DashBoardCategoryViewModel @Inject constructor(
         addCart(cartList!![0])
     }
 
-
+    // this method manages all the calculations like, taxes, service charge, discount, surcharge /cash discount on ADD/Update/Delete items from cart
     fun newCartLogicModifier(
         cartList: List<CartModel>?,
         item: TbItem?,
@@ -604,12 +605,15 @@ class DashBoardCategoryViewModel @Inject constructor(
             if (item != null) {
                 if (type == UPDATE) {
                     cartModel?.items?.forEach { items ->
+                        // calculated taxes of all items
                         cartModel = taxBifurcationCalculation(items, cartModel!!, type, false)
                     }
                 } else {
+                    // calculated taxes of all items
                     cartModel = taxBifurcationCalculation(item, cartModel!!, type, false)
                 }
             } else if (dineInList.isNotEmpty()) {
+                // tax calculation for dine in items
                 dineInList.forEach { dineInModel ->
                     dineInModel.items.forEach { itemData ->
                         cartModel = taxBifurcationCalculation(itemData, cartModel!!, type, false)
@@ -756,6 +760,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                 }
                             }
 
+                            // to validate item quantity for dine in cart(each item quantity must be less than or equal 1000)
                             if (!isDineInItem1000) {
                                 list.forEach {
                                     if (it.id == item?.id && it.itemId == item.itemId) {
@@ -784,6 +789,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                     if (model != null) {
                                         if (index != -1) {
                                             if (item != null) {
+                                                // To add/update item data of cart
                                                 model.name = item.name
                                                 model.itemQuantity =
                                                     model.itemQuantity + item.itemQuantity
@@ -842,6 +848,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                 } else if (index != -2) {
                                     Log.d(TAG, "cartLogic: " + index)
                                     if (item != null) {
+                                        // to update modifier data of any item
                                         item.singleItemPrice = item.price
                                         item.modifiers.forEach { it ->
 //                                            it.modifier_quantity = it.itemQuantity
@@ -918,6 +925,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                         }
 
                                     } else {
+                                        // Check if added or updated item matches with any other existing item in the cart. If matches with any other item than combine items
                                         if (list[i].id == item.id && list[i].itemId == item.itemId && list[i].timeStamp == item.timeStamp) {
                                             Log.d(TAG, "cartLogic: " + i)
                                             index = i
@@ -962,6 +970,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                 Log.e(TAG, "Itis NotMinus  ")
 
                             } else if (index != -1) {
+                                // if item matches with any other item in cart then update quantity and other data
                                 val model = list.get(index)
                                 Log.d(TAG, "cartLogic: " + index)
                                 if (model != null) {
@@ -1027,6 +1036,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                                 }
                             }
                         } else if (type == DELETE) {
+                            // to remove item from cart. Removed item will be sent with _destroy = true flag
                             var list: ArrayList<TbItem> = arrayListOf()
                             if (item != null) {
                                 list = dineInList[item.headerPositionDinein].items
@@ -1141,6 +1151,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                 } else {
 
                     if (type == DELETE) {
+                        // deletes whole cart
                         deleteCart()
                     } else {
 
@@ -1158,6 +1169,7 @@ class DashBoardCategoryViewModel @Inject constructor(
                         }
 
                         if (cartModel != null) {
+                            // Update cart with modification
                             addCart(cartModel!!)
                         }
                     }
@@ -3349,6 +3361,7 @@ class DashBoardCategoryViewModel @Inject constructor(
         return totaltaxtemp
     }
 
+    //  To calculate each item tax
     private fun taxBifurcationCalculation(
         item: TbItem, cartModel: CartModel, type: String, orderTaxID: Boolean
     ): CartModel {
