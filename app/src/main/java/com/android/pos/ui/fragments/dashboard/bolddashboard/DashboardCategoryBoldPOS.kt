ced17141daeff5a8649daef2a4bebe16e1e3afe4
udayinternetsoft <uday@internetsoft.com>
2023-12-06 15:22:06 +0530

    BIS - 3500 - Added Loading Animation after clicking sync button

--- app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
+++ app/src/main/java/com/android/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
@@ -19,6 +19,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.*
@@ -85,7 +86,9 @@ import com.android.pos.utils.callback.ItemClickListner
 import com.android.pos.utils.callback.ItemListner
 import com.android.pos.utils.extensions.alert
 import com.android.pos.utils.extensions.gone
+import com.android.pos.utils.extensions.hide
 import com.android.pos.utils.extensions.runOnUiThread
+import com.android.pos.utils.extensions.show
 import com.android.pos.utils.extensions.visible
 import com.android.pos.utils.printer.PrinterClass
 import com.android.pos.utils.scanner.helpers.ScannerAppEngine
@@ -227,9 +230,22 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
         }
     }
 
+
+
     private fun autoSyncObserver(){
+        val rotateAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.rotate_animation)
         viewModel.autoSyncEnabled.observe(viewLifecycleOwner){
-            binding.layoutHeader.imgSync.visibility = if(it) View.VISIBLE else View.INVISIBLE
+            binding.layoutHeader.imgSync.visibility = if(it) {
+                View.VISIBLE
+            }else{
+                View.GONE
+            }
+
+            if(it){
+                binding.layoutHeader.syncProgressBar.visibility = View.GONE
+            }else {
+                binding.layoutHeader.syncProgressBar.visibility = View.VISIBLE
+            }
         }
     }
 
