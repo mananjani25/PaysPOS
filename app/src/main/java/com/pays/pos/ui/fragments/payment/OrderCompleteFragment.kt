a4c22aca661eef8b1673fba85006381402920a3a
rahulsharma860 <rahul.shwork@gmail.com>
2024-02-12 15:39:12 +0530

    Solved BIS-3573

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -9231,7 +9231,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                     .styleBold(true)
                                     .actionPrintText(
                                         content = if (kitchenSettingModel.showCustomerName && (receiptModel?.order?.customer?.firstName != null || receiptModel?.order?.customer?.lastName != null)) {
-                                            "Customer Details\n"
+                                            "\nCustomer Details\n"
                                         } else ""
                                     )
                             )
@@ -9271,11 +9271,27 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                                 0
                                             ) != null
                                         ) {
-                                            MethodUtils.formatPhoneNumber(
+
+                                            var phoneNumber=receiptModel?.order?.customer?.phones?.get(
+                                                0
+                                            )?.phoneNumber.toString()
+                                            if (phoneNumber.length != 10) {
+                                                // Handle invalid input (must be 10 digits)
+                                                "Invalid phone number"
+                                            }
+
+                                            val areaCode = phoneNumber.substring(0, 3)
+                                            val firstPart = phoneNumber.substring(3, 6)
+                                            val secondPart = phoneNumber.substring(6)
+
+                                            "($areaCode)$firstPart-$secondPart"
+
+
+                                           /* MethodUtils.formatPhoneNumber(
                                                 receiptModel?.order?.customer?.phones?.get(
                                                     0
                                                 )?.phoneNumber.toString()
-                                            )
+                                            )*/
                                         } else ""
                                     )
                             )
