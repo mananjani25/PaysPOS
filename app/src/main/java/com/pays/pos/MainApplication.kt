3435ca12444933dd315f8c83072b1b9e6c25a4f3
rahulsharma860 <rahul.shwork@gmail.com>
2024-02-15 16:01:59 +0530

    1. Added test comment in MainApplication.kt

--- app/src/main/java/com/pays/pos/MainApplication.kt
+++ app/src/main/java/com/pays/pos/MainApplication.kt
@@ -75,6 +75,7 @@ class MainApplication : Application() {
         createNotificationChannel()
         sdkHandler = SDKHandler(this, true)
         init()
+        /*Test comment*/
     }
 
     fun init(){
