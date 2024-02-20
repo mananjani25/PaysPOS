f3d8967332d388e30748108df6d6bc129cb08c5a
rahulsharma860 <rahul.shwork@gmail.com>
2024-02-20 22:38:19 +0530

    1. Cart empty solved.

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -2574,7 +2574,7 @@ class CartFragment(
 
         binding.tvPayNow.setOnClickListener {
             runBlocking {
-              //  delay(300)
+                delay(500)
                 if (prefProvider.getValue(ORDER_TYPE, "") == OPEN_ORDER) {
 
                     prefProvider.setValueboolean(OPEN_ORDER_DIRECT_PAY, true)
