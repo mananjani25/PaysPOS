fedd890a96dcdc1a32319582c364304f1ef48b13
rahulinternetsoft <rahul@internetsoft.com>
2024-05-31 17:13:56 +0530

    1. Handled the crash in the AllOrdersCountsFragment.kt.

--- app/src/main/java/com/pays/pos/ui/fragments/allorders/AllOrdersCountsFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/allorders/AllOrdersCountsFragment.kt
@@ -478,8 +478,15 @@ class AllOrdersCountsFragment(val tabPosition: Int) : Fragment() {
     }
 
     private fun loadFragment(frag: Fragment) {
-        val fm: FragmentManager = requireActivity().supportFragmentManager
-        fm.beginTransaction().replace(binding.frameLayout.id, frag).commit()
+       try{
+           activity?.let {
+               val fm: FragmentManager = it.supportFragmentManager
+               fm.beginTransaction().replace(binding.frameLayout.id, frag).commit()
+           }
+
+       }catch (e:Exception){
+
+       }
     }
 
     private fun setAdapter(pos: Int) {
