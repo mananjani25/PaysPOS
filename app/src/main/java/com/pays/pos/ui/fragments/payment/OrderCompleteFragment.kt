a7f8bc88a3828e37c454ba224901778faa9b1d68
rahulinternetsoft <rahul@internetsoft.com>
2024-08-20 20:15:59 +0530

    DO_NOT_MERGE

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -10263,7 +10263,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
             }
 
 
-        } else if (data.name.contains("TSP", ignoreCase = true)) {
+        } else if (((data.name.contains("TSP", ignoreCase = true))) || ((data.name.contains("SP", ignoreCase = true)))) {
             settings = StarConnectionSettings(InterfaceType.Lan, data.macAddress)
             printer = StarPrinter(settings, requireContext())
 
