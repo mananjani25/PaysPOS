257c8700b4d63a0c3fdd4fec66fd193eb1efe86e
rahulinternetsoft <rahul@internetsoft.com>
2024-10-02 17:45:25 +0530

    Solved the crash issue when order is saved.

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
@@ -5459,6 +5459,7 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
         receiptModel: CreateOrderResponse.Data,
         cartModel: CartModel?
     ) {
+
         try {
 
             PrintSunmiUtils.fontSize(kitchenSettingModel.fonts)
@@ -5617,15 +5618,22 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
 
             viewModel.downloadFinished(false)
 
-            if (findNavController().currentDestination?.id == R.id.dashboardCategoryBoldPOS)
-                findNavController().navigate(R.id.action_dashboardCategoryBoldPOS_to_allOrdersFragment)
-
+            runOnUiThread(object :Runnable{
+                override fun run() {
+                    if (findNavController().currentDestination?.id == R.id.dashboardCategoryBoldPOS)
+                        findNavController().navigate(R.id.action_dashboardCategoryBoldPOS_to_allOrdersFragment)
+                }
+            })
 
         } catch (e: Exception) {
             e.printStackTrace()
             viewModel.downloadFinished(false)
-            if (findNavController().currentDestination?.id == R.id.dashboardCategoryBoldPOS)
-                findNavController().navigate(R.id.action_dashboardCategoryBoldPOS_to_allOrdersFragment)
+            runOnUiThread(object:Runnable{
+                override fun run() {
+                    if (findNavController().currentDestination?.id == R.id.dashboardCategoryBoldPOS)
+                        findNavController().navigate(R.id.action_dashboardCategoryBoldPOS_to_allOrdersFragment)
+                }
+            })
         }
 
     }
