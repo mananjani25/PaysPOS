3e12fe01b357e1c6b0ff53f04209fb07ca41756a
rahulinternetsoft <rahul@internetsoft.com>
2024-10-02 14:46:00 +0530

    Solved BIS-4528, enclosed the code by checking if the variable is initialized.

--- app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
@@ -310,21 +310,23 @@ class TransactionDetailsFragment : Fragment() {
         }
 
         binding.tvtipadd.setOnClickListener {
-            if (!paymentDetailsResponse.data.payable_type.equals(
-                    "GiftCard",
-                    true
-                ) && !paymentDetailsResponse.data.payable_type.equals(
-                    "Invoice", true
-                )
-            ) {
-                val bundle = Bundle()
-                bundle.putDouble("totalTip", paymentDetailsResponse.data.tips)
-                bundle.putBoolean("isFromTransaction", true)
-                paymentDetailsResponse.data.amount.let { bundle.putDouble("totalPrice", it) }
-                findNavController().navigate(
-                    R.id.action_transactionDetailsFragment_to_tipdialog,
-                    bundle
-                )
+            if (this::paymentDetailsResponse.isInitialized) {
+                if (!paymentDetailsResponse.data.payable_type.equals(
+                        "GiftCard",
+                        true
+                    ) && !paymentDetailsResponse.data.payable_type.equals(
+                        "Invoice", true
+                    )
+                ) {
+                    val bundle = Bundle()
+                    bundle.putDouble("totalTip", paymentDetailsResponse.data.tips)
+                    bundle.putBoolean("isFromTransaction", true)
+                    paymentDetailsResponse.data.amount.let { bundle.putDouble("totalPrice", it) }
+                    findNavController().navigate(
+                        R.id.action_transactionDetailsFragment_to_tipdialog,
+                        bundle
+                    )
+                }
             }
         }
         setFragmentResultListener("request_key_tips") { requestKey: String, bundle: Bundle ->
