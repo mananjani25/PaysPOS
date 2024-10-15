32ee4b5bc0438e9aa7794938b32dd3373fa72af5
gopalrajp9 <gopalrajp9@gmail.com>
2024-10-15 22:44:13 +0530

    Bug Fixes. BIS-4499,4523

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
@@ -6213,7 +6213,10 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
                     type = Constants.AVAILABLE,
                     uuid = UUID.randomUUID()
                 )
-                setupInnerPrinterAttributes(innerPrinterModel)
+                CoroutineScope(Dispatchers.IO).launch {
+                    delay(4000)
+                    setupInnerPrinterAttributes(innerPrinterModel)
+                }
 
             } else {
                 binding.maskLayout?.gone()
@@ -6255,30 +6258,52 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
             .post(MessageEvent("${Constants.LINE_BREAK_TAB} DashboardCategoryBoldPOS.kt_ setupInnerPrinterAttributes_createPrinter= ${Gson().toJson(createPrinter)}"))
         disableTouch()
         setCreatePrintersObserver()
-        printerViewModel.createPrinter(createPrinter)
-    }
-
-    private fun setCreatePrintersObserver() {
-        printerViewModel.disableCompleteTouch.observe(viewLifecycleOwner,object:Observer<Event<Boolean>>{
-            override fun onChanged(t: Event<Boolean>?) {
-                t?.getContentIfNotHandled()?.let {
-                    if (it){
-                        enableTouch()
-                    }else{
-                        disableTouch()
-                    }
-                }
-            }
+        runOnUiThread(kotlinx.coroutines.Runnable {
+            printerViewModel.createPrinter(createPrinter)
         })
     }
 
+    private fun setCreatePrintersObserver() {
+
+        runOnUiThread(kotlinx.coroutines.Runnable {
+            printerViewModel.disableCompleteTouch.observe(viewLifecycleOwner,
+                object : Observer<Event<Boolean>> {
+                    override fun onChanged(t: Event<Boolean>?) {
+                        t?.getContentIfNotHandled()?.let {
+                            if (it) {
+                                enableTouch()
+                            } else {
+                                disableTouch()
+                            }
+                        }
+                    }
+                })
+        })
+
+    }
+
     private fun disableTouch(){
-        requireActivity().window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        try {
+            runOnUiThread(kotlinx.coroutines.Runnable {
+            requireActivity().window.setFlags(
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+            );
+            })
+        }catch (e:Exception)
+        {
+            e.printStackTrace()
+        }
     }
 
     private fun enableTouch(){
-        requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        try {
+            runOnUiThread(kotlinx.coroutines.Runnable {
+            requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            })
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
     }
 
     private fun printByBluTooth(content: String) {
