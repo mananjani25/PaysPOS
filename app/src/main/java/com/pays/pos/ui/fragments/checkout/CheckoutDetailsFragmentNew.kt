90c1bc71f73c5201d38178032ecfe9574ff16862
rahulinternetsoft <rahul@internetsoft.com>
2024-11-11 16:33:34 +0530

    1. Added a solution for BIS-4728. (Continuous loading issue.)

--- app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -1438,16 +1438,25 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
                                     R.id.action_paymentBoldPosFragment_to_orderComplete,
                                     bundle
                                 )
+                            }else{
+                                runOnUiThread(Runnable {
+                                    dismissProgressDialog()
+                                })
                             }
 
                         }
                     }
                 } else {
+                    runOnUiThread(Runnable {
+                        dismissProgressDialog()
+                    })
+
                     AlertUtils.showCustomAlertWithListenerWithOK(
                         requireContext(),
                         message = it.message
                     ) { _, _ ->
                     }
+
                 }
 
 
@@ -3859,6 +3868,7 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
                                 }", true
                             )
                         )
+                        cartList = Gson().fromJson(model, CartModel::class.java)
                         dashboardViewModel.cartModel =
                             Gson().fromJson(model, CartModel::class.java)
                     }catch (e:Exception){
@@ -3866,7 +3876,11 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
                             CoroutineScope(Dispatchers.IO).async { dashboardViewModel.getAllCartModels() }
                                 .await()
                         if (models.isNotEmpty()) {
-                            dashboardViewModel.cartModel = models.get(0)
+                            dashboardViewModel.cartModel = models.last()
+                            cartList=models.last()
+                        }else{
+                            /*Continuous loading shall occur due to the cartModel null */
+
                         }
                     }
                 }
@@ -3887,8 +3901,20 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
             prefProvider.setValueboolean(DO_PRINT, true)
         }
 
-
+        EventBus.getDefault().post(
+            MessageEvent(
+                "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_makePaymentCreditCard() -> cartList -> ${
+                    Gson().toJson(cartList)
+                }", true
+            )
+        )
         if (cartList == null || cartList?.items == null || cartList?.items?.isEmpty() == true) {
+
+            EventBus.getDefault().post(
+                MessageEvent(
+                    "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_makePaymentCreditCard() -> null Case"
+                ))
+
             var items: ArrayList<TbItem>? = ArrayList()
             /*  for (item in dashboardViewModel.currentCartItems) {*/
             for (item in dashboardViewModel.currentCartItems) {
@@ -4085,6 +4111,8 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
             /*Adding the deleted items*/
             cartList?.items = items
         }
+
+
         prefProvider.setValue(Constants.OLD_ITEM, "")
         prefProvider.setValue(Constants.OLD_ITEM_BASE, "")
         prefProvider.setValue(Constants.OLD_ITEM_BASE_CUSTOM_ITEM, "")
