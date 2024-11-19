b8f90a5f2eb29dedeecda9cffa4626b5c0bf2de4
rahulinternetsoft <rahul@internetsoft.com>
2024-11-19 22:05:13 +0530

    Solved BIS-4253

--- app/src/main/java/com/pays/pos/ui/fragments/report/ReportSummaryFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/report/ReportSummaryFragment.kt
@@ -129,16 +129,26 @@ class ReportSummaryFragment : Fragment(), AdapterView.OnItemSelectedListener {
         }
 
         endTime = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-            val timecalender = Calendar.getInstance()
-            timecalender.set(Calendar.HOUR_OF_DAY, hour)
-            timecalender.set(Calendar.MINUTE, minute)
-            viewModel.endDate.value = timeCalculateForStartEndTime(hour, minute, "isend")
-            if (differnceTrue(viewModel.endDate.value!!, viewModel.startDate.value) <= 30) {
-                viewModel.getReportSummary()
-            } else {
+            var fromDate = SimpleDateFormat("dd/MM/yyyy HH:mm").parse(viewModel.startDate.value).getTime() / 1000
+            var endDate = SimpleDateFormat("dd/MM/yyyy HH:mm").parse(timeCalculateForStartEndTime(hour, minute, "isend")).getTime() / 1000
+            if (fromDate<=endDate){
+                val timecalender = Calendar.getInstance()
+                timecalender.set(Calendar.HOUR_OF_DAY, hour)
+                timecalender.set(Calendar.MINUTE, minute)
+                viewModel.endDate.value = timeCalculateForStartEndTime(hour, minute, "isend")
+                if (differnceTrue(viewModel.endDate.value!!, viewModel.startDate.value) <= 30) {
+                    viewModel.getReportSummary()
+                } else {
+                    AlertUtils.showCustomAlertWithListenerWithOK(
+                        requireActivity(),
+                        "Please Select date in 30 Days."
+                    ) { _, _ ->
+                    }
+                }
+            }else{
                 AlertUtils.showCustomAlertWithListenerWithOK(
                     requireActivity(),
-                    "Please Select date in 30 Days."
+                    "The end date cannot be earlier than the start date. Please select a valid date range."
                 ) { _, _ ->
                 }
             }
