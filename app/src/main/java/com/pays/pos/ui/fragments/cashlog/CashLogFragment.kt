3a3883e370167bc52a95502de88eb1625c24ff85
rahulinternetsoft <rahul@internetsoft.com>
2024-11-21 15:51:09 +0530

    1. Solved BIS-4253.

--- app/src/main/java/com/pays/pos/ui/fragments/cashlog/CashLogFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/cashlog/CashLogFragment.kt
@@ -129,21 +129,31 @@ class CashLogFragment : Fragment(), AdapterView.OnItemSelectedListener {
         }
 
         endTime = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-            val timecalender = Calendar.getInstance()
-            timecalender.set(Calendar.HOUR_OF_DAY, hour)
-            timecalender.set(Calendar.MINUTE, minute)
-            viewModel.endDate.value = timeCalculateForStartEndTime(hour, minute, "isend")
-            if (differnceTrue(viewModel.endDate.value!!, viewModel.startDate.value) <= 30) {
-                currentPage = 1
-                adapter.clear()
-                viewModel.apiCallTimeSheet(
-                    getTerminalId(binding.spTerminals.selectedItemPosition).toString(),
-                    currentPage
-                )
-            } else {
+            var fromDate = SimpleDateFormat("dd/MM/yyyy hh:mm a").parse(viewModel.startDate.value).getTime() / 1000
+            var endDate = SimpleDateFormat("dd/MM/yyyy hh:mm a").parse(timeCalculateForStartEndTime(hour, minute, "isend")).getTime() / 1000
+            if (fromDate<=endDate) {
+                val timecalender = Calendar.getInstance()
+                timecalender.set(Calendar.HOUR_OF_DAY, hour)
+                timecalender.set(Calendar.MINUTE, minute)
+                viewModel.endDate.value = timeCalculateForStartEndTime(hour, minute, "isend")
+                if (differnceTrue(viewModel.endDate.value!!, viewModel.startDate.value) <= 30) {
+                    currentPage = 1
+                    adapter.clear()
+                    viewModel.apiCallTimeSheet(
+                        getTerminalId(binding.spTerminals.selectedItemPosition).toString(),
+                        currentPage
+                    )
+                } else {
+                    AlertUtils.showCustomAlertWithListenerWithOK(
+                        requireActivity(),
+                        "Please Select date in 30 Days."
+                    ) { _, _ ->
+                    }
+                }
+            }else{
                 AlertUtils.showCustomAlertWithListenerWithOK(
                     requireActivity(),
-                    "Please Select date in 30 Days."
+                    "The end date cannot be earlier than the start date. Please select a valid date range."
                 ) { _, _ ->
                 }
             }
