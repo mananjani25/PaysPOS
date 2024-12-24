465f29d2c7c06e0ab818061689c41a7efa2a7faf
rahulinternetsoft <rahul@internetsoft.com>
2024-12-24 18:30:15 +0530

    Merging the Dejavoo.

--- app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -3720,6 +3720,428 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
         }
     }
 
+    private fun makePaymentCreditCardDejavoo(txnid: String?, extData: String?) {
+        paymentviewModel.dejavooRefTxnId = txnid
+//        paymentviewModel.valorTransactionNumber = transactionNumber
+        paymentAmount -= tipAmount
+        paymentAmount = MethodUtils.roundOffAmountDouble(paymentAmount)
+        paymentType = "Card"
+        Log.d(
+            "LOADER::",
+            "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+        )
+
+//        LogUtil.logE(TAG, "cartList:  ${Gson().toJson(cartList)}")
+//        LogUtil.logE(TAG, "cartListcartItems:  ${Gson().toJson(cartItems)}")
+        if (orderId != -1 && orderId != 0) {
+            paymentviewModel.updateOrder(
+                true,
+                orderId,
+                paymentId,
+                paymentOfflineId,
+                orderOfflineId
+            )
+        } else {
+            paymentviewModel.updateOrder(false, null, null, "", "")
+        }
+        paymentviewModel.saveOrder(false)
+        var cartModel: CartModel? = null
+        try {
+            cartModel =
+                Gson().fromJson<CartModel?>(
+                    prefProvider.getValue("CART_MODEL1", ""),
+                    CartModel::class.java
+                )
+        } catch (e: Exception) {
+
+        }
+        var cartModel2: CartModel? = null
+        try {
+            cartModel2 = Gson().fromJson<CartModel?>(
+                prefProvider.getValue("CART_MODEL2", ""),
+                CartModel::class.java
+            )
+        } catch (e: Exception) {
+
+        }
+
+        if (cartList == null) {
+            Log.d(
+                "LOADER::",
+                "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+            )
+
+            CoroutineScope(Dispatchers.Main).launch {
+                getCartModelsList()
+            }
+
+            if (cartModel != null) {
+                dashboardViewModel.cartModel = cartModel
+                cartList = cartModel
+            } else if (cartModel2 != null) {
+                dashboardViewModel.cartModel = cartModel2
+                cartList = cartModel2
+            } else if (dashboardViewModel.cartModel == null) {
+                runBlocking {
+                    try {
+                        var model =
+                            CoroutineScope(Dispatchers.IO).async { dashboardViewModel.getCartModelBackup() }
+                                .await().last().data
+
+                        EventBus.getDefault().post(
+                            MessageEvent(
+                                "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_CART_BACKUP_MODEL -> model -> ${
+                                    Gson().toJson(model)
+                                }", true
+                            )
+                        )
+                        cartList = Gson().fromJson(model, CartModel::class.java)
+                        dashboardViewModel.cartModel =
+                            Gson().fromJson(model, CartModel::class.java)
+                        Log.d(
+                            "LOADER::",
+                            "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+                        )
+
+                    } catch (e: Exception) {
+                        var models =
+                            CoroutineScope(Dispatchers.IO).async { dashboardViewModel.getAllCartModels() }
+                                .await()
+                        if (models.isNotEmpty()) {
+                            dashboardViewModel.cartModel = models.last()
+                            cartList = models.last()
+                            Log.d(
+                                "LOADER::",
+                                "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+                            )
+
+                        } else {
+                            Log.d(
+                                "LOADER::",
+                                "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+                            )
+
+                            /*Continuous loading shall occur due to the cartModel null */
+
+                        }
+                    }
+                }
+            }
+        }
+
+        Log.d(
+            "LOADER::",
+            "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+        )
+
+        oldItems = prefProvider.getValue(Constants.OLD_ITEM_BASE_CUSTOM_ITEM, "")
+        val listType = object : TypeToken<java.util.ArrayList<TbCartItem>>() {}.type
+        lateinit var oldCartItemsList: ArrayList<TbCartItem>
+
+        if (oldItems.isNotEmpty()) {
+
+            val oldCartItemsJson = prefProvider.getValue(Constants.OLD_ITEM_BASE_CUSTOM_ITEM, "")
+
+            oldCartItemsList = Gson().fromJson(oldCartItemsJson, listType) as ArrayList<TbCartItem>
+        } else {
+            prefProvider.setValueboolean(DO_PRINT, true)
+        }
+
+        EventBus.getDefault().post(
+            MessageEvent(
+                "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_makePaymentCreditCard() -> cartList -> ${
+                    Gson().toJson(cartList)
+                }", true
+            )
+        )
+        if (cartList == null || cartList?.items == null || cartList?.items?.isEmpty() == true) {
+            Log.d(
+                "LOADER::",
+                "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+            )
+
+            EventBus.getDefault().post(
+                MessageEvent(
+                    "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_makePaymentCreditCard() -> null Case"
+                )
+            )
+
+            var items: ArrayList<TbItem>? = ArrayList()
+            /*  for (item in dashboardViewModel.currentCartItems) {*/
+            for (item in dashboardViewModel.currentCartItems) {
+                var tbItem = TbItem()
+                tbItem.apply {
+
+                    isItemEdited = item.isItemEdited
+
+                    itemQuantity = item.itemQuantity
+                    id = item.id
+                    cartItemId = item.cartItemId
+                    itemId = item.itemId
+                    categoryId = item.categoryId
+                    categoryName = item.categoryName
+                    createdAt = item.createdAt
+                    customItemCount = item.customItemCount
+                    dineInSort = item.dineInSort
+                    customItemID = item.customItemCount
+                    discountId = item.discountId
+                    discountPrice = item.discountPrice
+                    discountType = item.discountType
+                    guestItemId = item.guestItemId
+                    headerPositionDinein = item.headerPositionDinein
+                    hide_status = item.hide_status
+                    isHide = item.isHide
+                    imageUrl = item.imageUrl
+                    isChecked = item.isChecked
+                    isDeleted = item.isDeleted
+                    isDestroy = item.isDestroy
+                    isEdited = item.isEdited
+                    isFired = item.isFired
+                    isManualSales = item.isManualSales
+                    isPaid = item.isPaid
+                    itemOriginalModifiersList = item.itemOriginalModifiersList
+                    modifier_set_ids = item.modifier_set_ids
+                    modifiers = item.modifiers
+                    name = item.name
+                    note = item.note
+                    manualSaleId = item.manualSaleId
+                    optionSets = item.optionSets
+                    website_hide_status = item.website_hide_status
+                    variationsAttributes = item.variationsAttributes
+                    updatedAt = item.updatedAt
+                    timeStamp = item.timeStamp
+                    thumbImageUrl = item.thumbImageUrl
+                    taxes = item.taxes
+                    sort = item.sort
+                    sku = item.sku
+                    singleItemPrice = item.singleItemPrice
+                    shortDescription = item.shortDescription
+                    reorder = item.reorder
+                    quantity = item.quantity
+                    price = item.price
+                    orderItemId = item.orderItemId
+
+                    try {
+                        if (oldCartItemsList.isNotEmpty()) { // Order is updated
+
+                            prefProvider.setValueboolean(Constants.DO_PRINT, true)
+
+                            oldCartItemsList.forEach { oldItem ->
+                                if ((oldItem.cartItemId == item.cartItemId) &&
+                                    (oldItem.categoryId == item.categoryId) &&
+                                    (oldItem.employeeID == item.employeeID) &&
+                                    (oldItem.itemId == item.itemId) &&
+                                    (oldItem.name.equals(item.name))
+                                ) {
+
+                                    if (item.itemQuantity != oldItem.itemQuantity) {
+                                        isItemEdited = true
+                                    }
+
+                                }
+                            }
+                        }
+                    } catch (e: Exception) {
+                        //order is not updated , its new order
+                    }
+                }
+
+                items!!.add(tbItem)
+
+            }
+
+            if (oldItems.isNotEmpty()) {
+
+                for (item in dashboardViewModel.currentCartItems) {
+                    try {
+                        if (oldItems.isNotEmpty()) {
+                            /*Added by Rahul to solve the modifiers not removing issue*/
+
+                            val notPresentItems =
+                                oldCartItemsList.filter { it.cartItemId != item.cartItemId }
+
+                            if (true) {
+                                var isFound = false
+
+                                for (notPresentData in oldCartItemsList) {
+                                    if (items != null) {
+                                        for (it in items) {
+                                            if (it.cartItemId == notPresentData.cartItemId) {
+                                                isFound = true
+                                                break
+                                            } else isFound = false
+                                        }
+                                    }
+                                    if (!isFound) {
+
+                                        prefProvider.setValueboolean(DO_PRINT, true)
+//                                        Not found
+                                        isFound = false
+
+                                        var tbItemDeleted = TbItem()
+                                        tbItemDeleted.id = notPresentData.id
+                                        tbItemDeleted.cartItemId = notPresentData.cartItemId
+                                        tbItemDeleted.itemId = notPresentData.itemId
+                                        tbItemDeleted.itemQuantity = notPresentData.itemQuantity
+                                        tbItemDeleted.categoryId = notPresentData.categoryId
+                                        tbItemDeleted.categoryName = notPresentData.categoryName
+                                        tbItemDeleted.createdAt = notPresentData.createdAt
+                                        tbItemDeleted.customItemCount =
+                                            notPresentData.customItemCount
+                                        tbItemDeleted.dineInSort = notPresentData.dineInSort
+                                        tbItemDeleted.customItemID = notPresentData.customItemCount
+                                        tbItemDeleted.discountId = notPresentData.discountId
+                                        tbItemDeleted.discountPrice = notPresentData.discountPrice
+                                        tbItemDeleted.discountType = notPresentData.discountType
+                                        tbItemDeleted.guestItemId = notPresentData.guestItemId
+                                        tbItemDeleted.headerPositionDinein =
+                                            notPresentData.headerPositionDinein
+                                        tbItemDeleted.hide_status = notPresentData.hide_status
+                                        tbItemDeleted.isHide = notPresentData.isHide
+                                        tbItemDeleted.imageUrl = notPresentData.imageUrl
+                                        tbItemDeleted.isChecked = notPresentData.isChecked
+                                        tbItemDeleted.isDeleted = notPresentData.isDeleted
+                                        tbItemDeleted.isDestroy = true
+                                        tbItemDeleted.isEdited = notPresentData.isEdited
+                                        tbItemDeleted.isFired = notPresentData.isFired
+                                        tbItemDeleted.isManualSales = notPresentData.isManualSales
+                                        tbItemDeleted.isPaid = notPresentData.isPaid
+                                        tbItemDeleted.itemOriginalModifiersList =
+                                            notPresentData.itemOriginalModifiersList
+                                        tbItemDeleted.quantity = notPresentData.quantity
+                                        tbItemDeleted.modifier_set_ids =
+                                            notPresentData.modifier_set_ids
+                                        tbItemDeleted.modifiers = notPresentData.modifiers
+                                        tbItemDeleted.name = notPresentData.name
+                                        tbItemDeleted.note = notPresentData.note
+                                        tbItemDeleted.manualSaleId = notPresentData.manualSaleId
+                                        tbItemDeleted.optionSets = notPresentData.optionSets
+                                        tbItemDeleted.website_hide_status =
+                                            notPresentData.website_hide_status
+                                        tbItemDeleted.variationsAttributes =
+                                            notPresentData.variationsAttributes
+                                        tbItemDeleted.updatedAt = notPresentData.updatedAt
+                                        tbItemDeleted.timeStamp = notPresentData.timeStamp
+                                        tbItemDeleted.thumbImageUrl = notPresentData.thumbImageUrl
+                                        tbItemDeleted.taxes = notPresentData.taxes
+                                        tbItemDeleted.sort = notPresentData.sort
+                                        tbItemDeleted.sku = notPresentData.sku
+                                        tbItemDeleted.singleItemPrice =
+                                            notPresentData.singleItemPrice
+                                        tbItemDeleted.shortDescription =
+                                            notPresentData.shortDescription
+                                        tbItemDeleted.reorder = notPresentData.reorder
+                                        tbItemDeleted.price = notPresentData.price
+                                        tbItemDeleted.orderItemId = notPresentData.orderItemId
+
+                                        items!!.add(tbItemDeleted)
+                                        break
+                                    }
+
+                                }
+
+                            }
+//                        for (notPresentItem in notPresentItems) {
+//                            if (item.itemId != notPresentItem.itemId) {
+
+
+//                            }
+//                        }
+                            Log.d("UNCOMMON:::", Gson().toJson(notPresentItems))
+
+
+                        }
+                    } catch (e: Exception) {
+                    }
+
+                }
+
+            }
+
+
+            /*Adding the deleted items*/
+            cartList?.items = items
+        }
+
+        Log.d(
+            "LOADER::",
+            "${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}"
+        )
+
+        prefProvider.setValue(Constants.OLD_ITEM, "")
+        prefProvider.setValue(Constants.OLD_ITEM_BASE, "")
+        prefProvider.setValue(Constants.OLD_ITEM_BASE_CUSTOM_ITEM, "")
+
+
+        val myRequest = cartList?.let {
+            txnid?.let {
+                if (RefNumber.isEmpty()) {
+                    RefNumber = it
+                    /*We are adding DEJAVOO in EXT DATA, This will change in further release, we will send the Dejavoo's Ext_Data in this Parameter */
+                    ExtData = "${Constants.DEJAVOO} : $extData?"
+                }
+            }
+
+            paymentviewModel.createOrderRequestForCard(
+                it,
+                subTotalPrice,
+                paymentAmount,
+                totalServiceCharge,
+                totalTax,
+                prefProvider.getValue(Constants.ORDER_TYPE, ""),
+                future_delivery_date,
+                future_delivery_time,
+                true,
+                totalDiscount,
+                tipAmount,
+                splitValue,
+                redeemLoyaltyInfo,
+                cashDiscountSurcharge,
+                true,
+                paymentType,
+                cardNumber,
+                cashDiscountType,
+                tipID,
+                GlobalUID,
+                RefNumber,
+                ExtData,
+                ECRRefNumber,
+                PAXtoken,
+                cardLastDigits,
+                cardTypeOfTransaction = EDCType
+            )
+        }
+        LogUtil.logE(TAG, "myRequestOriginal ${Gson().toJson(myRequest)}")
+        if (myRequest != null) {
+            paymentviewModel.totalPayAmount(paymentAmount)
+
+            if (prefProvider.getValue(Constants.GIFT_CARD_TYPE, "")
+                    .equals("digital", ignoreCase = true)
+            ) {
+                myRequest.order.orderTypeId = prefProvider.getValueInt(Constants.ORDER_TYPE_ID, -1)
+            }
+
+            EventBus.getDefault().post(
+                MessageEvent(
+                    "${Constants.LINE_BREAK_TAB} CheckoutDetailsFragmentNew.kt_makePaymentCreditCard() , myRequest -> ${
+                        Gson().toJson(myRequest)
+                    } _1"
+                )
+            )
+            Log.d("LOADER::","${Exception().stackTrace[0].fileName} -> ${Exception().stackTrace[0].lineNumber}")
+
+            /* //FILE ASSERTION
+             MainActivity.writeToFile(
+                 Gson().toJson(myRequest),
+                 "Pay_".plus(myRequest.order.offlineId.toString()),
+                 activity?.filesDir,
+                 activity!!
+             )*/
+
+            paymentAttributesRequest(myRequest)
+        }
+    }
+
     private fun startCashOrGiftCardTransaction(b: Boolean) {
 
     }
