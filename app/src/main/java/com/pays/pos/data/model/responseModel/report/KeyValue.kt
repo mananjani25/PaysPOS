a6a4235172f0040c32bb0a50a722fac356373a7d
rahulinternetsoft <148438452+rahulinternetsoft@users.noreply.github.com>
2024-12-27 21:13:59 +0530

    Revert "Release v1.27 5082"

--- app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValue.kt
+++ app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValue.kt
@@ -3,7 +3,6 @@ package com.pays.pos.data.model.responseModel.report
 import android.util.Log
 import android.view.View
 import com.google.gson.annotations.SerializedName
-import kotlin.math.absoluteValue
 
 data class KeyValue(
     @SerializedName("key")
@@ -78,22 +77,16 @@ data class KeyValue(
         }
 
 
-    fun showFormattedValue() =
-        if (value?.isEmpty() == true) "$0.00" else if ((value?.toDouble() ?: 0.0) < 0.0){ "-$${String.format(
-            "%.2f", value?.toDouble()?.absoluteValue ?: 0.0
-        )}" } else "$${String.format(
-            "%.2f", value?.toDouble()?.absoluteValue ?: 0.0
-        )}"
+    fun showFormattedValue() = if (value?.isEmpty() == true) "$0.00" else "$" + String.format(
+        "%.2f", value?.toDouble() ?: 0.0
+    )
 
     fun showName() =  value.toString()
 
     fun showDividerLine() = if(key?.contains("name",true)==true) View.VISIBLE else View.GONE
 
     private fun showFormattedValueMinus() =
-        /*if (value?.isEmpty() == true || (value=="0.0" || value=="0.00")) "$0.00" else "-$" + String.format(
-            "%.2f", value?.toDouble() ?: 0.0
-        )*/
         if (value?.isEmpty() == true || (value=="0.0" || value=="0.00")) "$0.00" else "-$" + String.format(
-            "%.2f", value?.toDouble()?.absoluteValue ?: 0.0
+            "%.2f", value?.toDouble() ?: 0.0
         )
 }
