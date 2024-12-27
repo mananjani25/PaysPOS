41789c660c21e2355a4f646cd97e565c5b238c09
Aman Singh Chauhan <aman.internetsoft.co>
2024-12-27 14:41:44 +0530

    BIS_5074 Solved.

--- app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValue.kt
+++ app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValue.kt
@@ -68,6 +68,9 @@ data class KeyValue(
             key?.trim().equals("Name") -> {
                 showName()
             }
+            key?.trim().equals("Count") -> {
+                showName()
+            }
             else -> {
                 showFormattedValue()
             }
