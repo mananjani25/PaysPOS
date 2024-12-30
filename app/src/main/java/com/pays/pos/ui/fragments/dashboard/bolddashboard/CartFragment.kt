1339508a9041086ae8d63344d47b42a19872222f
rahulinternetsoft <rahul@internetsoft.com>
2024-12-30 16:05:12 +0530

    Hidden the visibility of the Preauth.

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -393,7 +393,7 @@ class CartFragment : Fragment, MyCallback, DineInAdapter.DineInCallback, ItemCal
     // To check selected order type
     private fun checkOrderType() {
 
-        setUpPreAuthData()
+//        setUpPreAuthData()
 
 //        saveVisibility()
         if (prefProvider.getValue(ORDER_TYPE, "").isEmpty()) {
