ce17a8c4197f6cede65aa159e1cbad4ec74815a8
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-01-07 18:09:37 +0530

    Solved BIS-5134

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -11121,7 +11121,7 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                         if (order!!.payments.last().paymentType == "Cash") {
                                             val changeAmt = padLine(
                                                 "Change Amount",
-                                                "$" + MethodUtils.roundOffAmountString(((paidAmount + tipAmount) - order!!.payments.last().amount)),
+                                                "$" + MethodUtils.roundOffAmountString(((paidAmount + tipAmount) - (order!!.payments.last().amount + tipAmount))),
                                                 if (customerSettingModel.fonts == LARGE) 23 else 48
                                             ).toString()
 
