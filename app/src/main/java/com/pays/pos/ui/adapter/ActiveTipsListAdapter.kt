a1169800fd21667e2f873f50be90c653dfe6b825
Aman Singh Chauhan <aman.internetsoft.co>
2025-01-17 18:14:27 +0530

    BIS 5257 Solved.

--- app/src/main/java/com/pays/pos/ui/adapter/ActiveTipsListAdapter.kt
+++ app/src/main/java/com/pays/pos/ui/adapter/ActiveTipsListAdapter.kt
@@ -47,7 +47,7 @@ class ActiveTipsListAdapter() :
 
                 //Show only whole number if there is 00 after decimal.
                 val tipRate = model.rate.toString()
-                val formattedValue = if (tipRate.substringAfter(".") == "00") {
+                val formattedValue = if (tipRate.substringAfter(".").equals("00",true)) {
                     tipRate.substringBefore(".") // Print only the integer part
                 } else {
                     tipRate // Print the full value
