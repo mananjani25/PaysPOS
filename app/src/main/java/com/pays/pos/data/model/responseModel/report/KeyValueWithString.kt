9d6bf45b89b468772e1ccd44907db6a93c66f575
rahulinternetsoft <rahul@internetsoft.com>
2025-01-20 18:09:26 +0530

    Solved Report amount issue., 5319

--- app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValueWithString.kt
+++ app/src/main/java/com/pays/pos/data/model/responseModel/report/KeyValueWithString.kt
@@ -1,6 +1,7 @@
 package com.pays.pos.data.model.responseModel.report
 
 import com.google.gson.annotations.SerializedName
+import kotlin.math.absoluteValue
 
 data class KeyValueWithString(
     @SerializedName("key")
@@ -10,12 +11,39 @@ data class KeyValueWithString(
 ) {
     fun showFormattedValue(): String {
         val formatted = try {
-            "$" + String.format(
-                "%.2f", value?.toDoubleOrNull()?:0
-            )
+            if (value?.toDouble()?:0.0 < 0.0){
+                var data=String.format(
+                    "%.2f", value?.toDouble()?.absoluteValue ?: 0
+                )
+                if (data.toDouble()==0.0){
+                    return "$" + data
+                }else {
+                    return "-$" + String.format(
+                        "%.2f", value?.toDouble()?.absoluteValue ?: 0
+                    )
+                }
+            }else {
+               return  "$" + String.format(
+                    "%.2f", value?.toDoubleOrNull() ?: 0
+                )
+            }
         } catch (e: Exception) {
             value ?: ""
         }
+
+        /* val formatted = try {
+            if (value?.toDouble()?:0.0 < 0.0){
+                return  "-$" + String.format(
+                    "%.2f", value?.toDouble()?.absoluteValue ?: 0
+                )
+            }else {
+               return  "$" + String.format(
+                    "%.2f", value?.toDoubleOrNull() ?: 0
+                )
+            }
+        } catch (e: Exception) {
+            value ?: ""
+        }*/
         return formatted
     }
 }
