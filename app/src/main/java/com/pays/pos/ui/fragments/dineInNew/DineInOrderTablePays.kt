31a82ddddff8e1364ac85a63ff6d8fd2d3fc7886
Aman Singh Chauhan <aman.internetsoft.co>
2025-01-20 16:46:07 +0530

    BIS 5326 Solved.

--- app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
@@ -1181,6 +1181,7 @@ class DineInOrderTablePays : Fragment(), DineInTableAdapter.DineInTableListner {
                 duplicateCurrentCartItem = arrayListOf()
                 deleteCartItems()
                 deleteCart()
+                clearCustomer()
             }
 
             findNavController().navigate(R.id.action_dineInOrderTable_to_dashboardCategoryNew)
