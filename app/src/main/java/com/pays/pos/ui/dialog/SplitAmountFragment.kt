9fa4230fa99f873a79856d3676611cf32029064c
Aman Singh Chauhan <aman.internetsoft.co>
2025-01-22 15:14:19 +0530

    Amount wise split.

--- app/src/main/java/com/pays/pos/ui/dialog/SplitAmountFragment.kt
+++ app/src/main/java/com/pays/pos/ui/dialog/SplitAmountFragment.kt
@@ -27,7 +27,7 @@ class SplitAmountFragment : DialogFragment(), View.OnClickListener, TextWatcher 
     private var isCustom: Boolean = false
     private var totalPrice: Double = 0.0
     private var splitValue: Int = 1
-    private var isAmountWiseSplit: Boolean = false
+//    private var isAmountWiseSplit: Boolean = false
     private lateinit var binding: DailogSplitAmountBinding
     var current = ""
 
@@ -50,14 +50,14 @@ class SplitAmountFragment : DialogFragment(), View.OnClickListener, TextWatcher 
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         isAmountWiseSplit = requireArguments().getBoolean("amountWiseSplit", false)
 
         if (isAmountWiseSplit) {
             binding.splitDialogTextView.text = "Amount to split"
         }
-    }
+    }*/
 
     private fun setupInputFilter() {
 
