90d48c1f73dca9f1fe13111960287426a98ac582
uday <uday@internetsoft.com>
2025-01-23 22:23:27 +0530

    DINE IN - BIS 5365

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -62,6 +62,7 @@ import com.pays.pos.data.remote.Constants.CASH_DISCOUNT_SURCHARGE
 import com.pays.pos.data.remote.Constants.CUSTOMER
 import com.pays.pos.data.remote.Constants.DINE_IN_ADAPTER_LIST
 import com.pays.pos.data.remote.Constants.DINE_IN_SUB_TOTAL_AMOUNT_BEFORE_PAYMENT
+import com.pays.pos.data.remote.Constants.DINE_IN_SUB_TOTAL_AMOUNT_BEFORE_PAYMENT_GUEST
 import com.pays.pos.data.remote.Constants.EMPLOYEE_NAME
 import com.pays.pos.data.remote.Constants.GIFT_CARD
 import com.pays.pos.data.remote.Constants.GUEST_POSITION
@@ -4554,10 +4555,20 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                      * Print Subtotal
                                      */
 
+                                    var fetchSubTotalFromPreference = guestPayment?.subTotal ?: 0.0
+
+                                    try {
+                                        fetchSubTotalFromPreference = prefProvider.getValue(
+                                            DINE_IN_SUB_TOTAL_AMOUNT_BEFORE_PAYMENT_GUEST,fetchSubTotalFromPreference.toString()).toDouble()
+                                    }catch (e:Exception) {
+
+                                        Log.e("DINE IN CRASH","${e.message.toString()}")
+                                    }
+
                                     val subTotalToPrint = padLine(
                                         "Sub Total",
                                         "$" + MethodUtils.roundOffAmountString(
-                                            guestPayment?.subTotal ?: 0.0
+                                            fetchSubTotalFromPreference
                                         ),
                                         48
                                     ).toString()
