3a87c890a139e7d694f5db07adb2f71d5110423b
rahulinternetsoft <rahul@internetsoft.com>
2025-01-27 19:59:47 +0530

    Enabled the PreAuth

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/CartFragment.kt
@@ -396,7 +396,7 @@ class CartFragment : Fragment, MyCallback, DineInAdapter.DineInCallback, ItemCal
     // To check selected order type
     private fun checkOrderType() {
 
-//        setUpPreAuthData()
+        setUpPreAuthData()
 
 //        saveVisibility()
         if (prefProvider.getValue(ORDER_TYPE, "").isEmpty()) {
