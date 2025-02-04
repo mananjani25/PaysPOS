edab40007f2747bb6f663b82b1701c67e0558e36
Aman Singh Chauhan <aman.internetsoft.co>
2025-02-04 21:11:46 +0530

    BIS 5294 for Valor and Dejavoo Solved.

--- app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -4251,8 +4251,11 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
                                 )
                             )
                         dismissProgressDialogWithAlert()
-                        dashboardViewModel.paymentInProgress.value = false
-                        dashboardViewModel.tipBeforeEnabled = true
+                        if (dashboardViewModel.customerGivenTipBefore.value == false) {
+                            dashboardViewModel.paymentInProgress.value = false
+                            dashboardViewModel.tipBeforeEnabled = true
+                            dashboardViewModel.removeMainCart.value = true
+                        }
                     }
                 )
             }
@@ -5034,6 +5037,11 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
                                     } "
                                 )
                             )
+                        if (dashboardViewModel.customerGivenTipBefore.value == false) {
+                            dashboardViewModel.paymentInProgress.value = false
+                            dashboardViewModel.tipBeforeEnabled = true
+                            dashboardViewModel.removeMainCart.value = true
+                        }
                     }
                 )
             }
