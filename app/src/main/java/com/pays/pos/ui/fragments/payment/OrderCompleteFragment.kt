fa416c50185113aadf570cf03c9060046535df19
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-02-06 23:00:39 +0530

    Solved PA1-I315

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -15287,7 +15287,9 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                         }
 
                     } else if (data.name.contains(LANDI_INNER_PRINTER, true)) {
-                        printKitchenFromLandiInner(data)
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            printKitchenFromLandiInner(data)
+                        }, 1000)
                     } else {
 
                         if (isNotPrinted) {
