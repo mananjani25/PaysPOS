f3483d1293e3595900ea7a0b184d674137982dd1
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-02-10 17:05:30 +0530

    Solved PA1-1486

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -15669,26 +15669,25 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                                             try {
                                                                 if (receiptModel?.order?.customer?.addresses?.isNotEmpty() == true) {
 
+                                                                    receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
+                                                                        printLeft(
+                                                                            it
+                                                                        )
+                                                                    }
 
-//                                receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
-//                                    PrintSunmiUtils.normalTextLarge(
-//                                        it
-//                                    )
-//                                }
-
-                                                                    receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
-                                                                        ?.forEach {
-
-                                                                            if (it.typeOfAddress.equals(
-                                                                                    BILLING_ADDRESS,
-                                                                                    ignoreCase = true
-                                                                                )
-                                                                            ) {
-                                                                                printLeft(
-                                                                                    it.fullAddress
-                                                                                )
-                                                                            }
-                                                                        }
+//                                                                    receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
+//                                                                        ?.forEach {
+//
+//                                                                            if (it.typeOfAddress.equals(
+//                                                                                    BILLING_ADDRESS,
+//                                                                                    ignoreCase = true
+//                                                                                )
+//                                                                            ) {
+//                                                                                printLeft(
+//                                                                                    it.fullAddress
+//                                                                                )
+//                                                                            }
+//                                                                        }
                                                                 }
                                                             } catch (e: Exception) {
                                                             }
@@ -17074,19 +17073,25 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
 
                                     if (receiptModel?.order?.customer?.addresses?.isNotEmpty() == true) {
 
-                                        receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
-                                            ?.forEach {
+                                        receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
+                                            PrintSunmiUtils.customerAddress(
+                                                it
+                                            )
+                                        }
 
-                                                if (it.typeOfAddress.equals(
-                                                        BILLING_ADDRESS,
-                                                        ignoreCase = true
-                                                    )
-                                                ) {
-                                                    PrintSunmiUtils.customerAddress(
-                                                        it.fullAddress
-                                                    )
-                                                }
-                                            }
+//                                        receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
+//                                            ?.forEach {
+//
+//                                                if (it.typeOfAddress.equals(
+//                                                        BILLING_ADDRESS,
+//                                                        ignoreCase = true
+//                                                    )
+//                                                ) {
+//                                                    PrintSunmiUtils.customerAddress(
+//                                                        it.fullAddress
+//                                                    )
+//                                                }
+//                                            }
 
 //                                receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
 //                                    PrintSunmiUtils.customerAddress(
@@ -17266,26 +17271,25 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                         try {
                                             if (receiptModel?.order?.customer?.addresses?.isNotEmpty() == true) {
 
+                                                receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
+                                                    PrintSunmiUtils.normalTextLarge(
+                                                        it
+                                                    )
+                                                }
 
-//                                receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
-//                                    PrintSunmiUtils.normalTextLarge(
-//                                        it
-//                                    )
-//                                }
-
-                                                receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
-                                                    ?.forEach {
-
-                                                        if (it.typeOfAddress.equals(
-                                                                BILLING_ADDRESS,
-                                                                ignoreCase = true
-                                                            )
-                                                        ) {
-                                                            PrintSunmiUtils.normalTextLarge(
-                                                                it.fullAddress
-                                                            )
-                                                        }
-                                                    }
+//                                                receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
+//                                                    ?.forEach {
+//
+//                                                        if (it.typeOfAddress.equals(
+//                                                                BILLING_ADDRESS,
+//                                                                ignoreCase = true
+//                                                            )
+//                                                        ) {
+//                                                            PrintSunmiUtils.normalTextLarge(
+//                                                                it.fullAddress
+//                                                            )
+//                                                        }
+//                                                    }
                                             }
                                         } catch (e: Exception) {
                                         }
@@ -17458,26 +17462,25 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                     try {
                                         if (receiptModel?.order?.customer?.addresses?.isNotEmpty() == true) {
 
+                                            receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
+                                                PrintSunmiUtils.normalTextLarge(
+                                                    it
+                                                )
+                                            }
 
-//                                receiptModel?.order?.customer?.addresses?.get(0)?.fullAddress?.let {
-//                                    PrintSunmiUtils.normalTextLarge(
-//                                        it
-//                                    )
-//                                }
-
-                                            receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
-                                                ?.forEach {
-
-                                                    if (it.typeOfAddress.equals(
-                                                            BILLING_ADDRESS,
-                                                            ignoreCase = true
-                                                        )
-                                                    ) {
-                                                        PrintSunmiUtils.normalTextLarge(
-                                                            it.fullAddress
-                                                        )
-                                                    }
-                                                }
+//                                            receiptModel?.order?.customer?.addresses?.filter { it.typeOfAddress == BILLING_ADDRESS }
+//                                                ?.forEach {
+//
+//                                                    if (it.typeOfAddress.equals(
+//                                                            BILLING_ADDRESS,
+//                                                            ignoreCase = true
+//                                                        )
+//                                                    ) {
+//                                                        PrintSunmiUtils.normalTextLarge(
+//                                                            it.fullAddress
+//                                                        )
+//                                                    }
+//                                                }
                                         }
                                     } catch (e: Exception) {
                                     }
