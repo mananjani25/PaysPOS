2192a790a6f0ba6f897b069130e0ad77f1e8e57e
rahulinternetsoft <rahul@internetsoft.com>
2025-02-11 15:36:27 +0530

    Solved PA1-I480.

--- app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dashboard/bolddashboard/DashboardCategoryBoldPOS.kt
@@ -680,15 +680,20 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
                 if (p0.toString() == " ") {
                     binding.layoutHeader.edtSearch.setText("")
                 }
-                if (requireActivity().supportFragmentManager.findFragmentById(R.id.frameLayout)?.javaClass?.name.equals(
-                        "com.pays.pos.ui.fragments.dashboard.bolddashboard.AddItemFragment", true
-                    )
-                ) {
+                activity?.let {
+                    it.findViewById<View>(R.id.frameLayout)?.let {
+                        if (requireActivity().supportFragmentManager.findFragmentById(R.id.frameLayout)?.javaClass?.name.equals(
+                                "com.pays.pos.ui.fragments.dashboard.bolddashboard.AddItemFragment",
+                                true
+                            )
+                        ) {
 
-                    requireActivity().supportFragmentManager.popBackStackImmediate(
-                        AddItemFragment.javaClass.getName(),
-                        FragmentManager.POP_BACK_STACK_INCLUSIVE
-                    )
+                            requireActivity().supportFragmentManager.popBackStackImmediate(
+                                AddItemFragment.javaClass.getName(),
+                                FragmentManager.POP_BACK_STACK_INCLUSIVE
+                            )
+                        }
+                    }
                 }
 
 
@@ -1839,17 +1844,21 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
                                 it.price = null
                             }
                         }
-                        val backStateName: String = AddItemFragment.javaClass.getName()
-                        val fragment = AddItemFragment.newInstance(
-                            item,
-                            this@DashboardCategoryBoldPOS,
-                            cartList,
-                            false
-                        )
-                        val fm: FragmentManager = requireActivity().supportFragmentManager
-                        fm.beginTransaction().add(binding.frameLayout.id, fragment)
-                            .setReorderingAllowed(true)
-                            .addToBackStack(backStateName).commit()
+                       activity?.let {
+                           it.findViewById<View>(R.id.frameLayout)?.let {
+                               val backStateName: String = AddItemFragment.javaClass.getName()
+                               val fragment = AddItemFragment.newInstance(
+                                   item,
+                                   this@DashboardCategoryBoldPOS,
+                                   cartList,
+                                   false
+                               )
+                               val fm: FragmentManager = requireActivity().supportFragmentManager
+                               fm.beginTransaction().add(binding.frameLayout.id, fragment)
+                                   .setReorderingAllowed(true)
+                                   .addToBackStack(backStateName).commit()
+                           }
+                       }
                         Log.e("Dashboard Tracking ", "Track Dashboard - 1317")
                     } else {
                         Log.d(TAG, "dineintest onItemSelected: ")
@@ -2281,39 +2290,49 @@ class DashboardCategoryBoldPOS() : Fragment(), ItemListner, ItemClickListner,
 
 
     override fun onItemUpdate(item: TbCartItem, position: Int) {
+        activity?.let {
+            it.findViewById<View>(R.id.frameLayout)?.let {
+                viewModel.isItemEditInProgress = true
+                viewModel.dineInHeaderPosition = item.guestIndexForDineIn ?: -1
 
-        viewModel.isItemEditInProgress = true
-        viewModel.dineInHeaderPosition = item.guestIndexForDineIn ?: -1
+                viewModel.isCartItemClicked = true
 
-        viewModel.isCartItemClicked = true
+                viewModel.cartItemModifiersBeforeUpdate = item.modifiers
 
-        viewModel.cartItemModifiersBeforeUpdate = item.modifiers
-
-        Log.e("ITEM MODIFIER SIZE", Gson().toJson(item.modifiers))
+                Log.e("ITEM MODIFIER SIZE", Gson().toJson(item.modifiers))
 
 
-        prefProvider.setValueInt(Constants.CAT_ID_SELECTED, item.categoryId)
-        val backStateName: String = AddItemFragment.javaClass.getName()
-        val fragment = AddItemFragment.newInstance(item, this, cartList, true, position)
-        val fm: FragmentManager = requireActivity().supportFragmentManager
-        fm.beginTransaction().add(binding.frameLayout.id, fragment).setReorderingAllowed(true)
-            .addToBackStack(backStateName).commit()
-        Log.e("Dashboard Tracking ", "Track Dashboard - 1652")
-        /*val frag: Fragment = AddItemFragment.newInstance(item, this, cartList, true)
-        loadCategoryFragment(frag)*/
+                prefProvider.setValueInt(Constants.CAT_ID_SELECTED, item.categoryId)
+                val backStateName: String = AddItemFragment.javaClass.getName()
+                val fragment = AddItemFragment.newInstance(item, this, cartList, true, position)
+                val fm: FragmentManager = requireActivity().supportFragmentManager
+                fm.beginTransaction().add(binding.frameLayout.id, fragment).setReorderingAllowed(true)
+                    .addToBackStack(backStateName).commit()
+                Log.e("Dashboard Tracking ", "Track Dashboard - 1652")
+                /*val frag: Fragment = AddItemFragment.newInstance(item, this, cartList, true)
+                loadCategoryFragment(frag)*/
+            }
+
+        }
+
     }
 
     override fun onCartItemUpdate(item: TbCartItem, position: Int) {
-        /*Added By Rahul - Update,  for checking the update for the given item, This will be the base object, with which we will compare the updated object*/
-        prefProvider.setValue(Constants.OPEN_ORDER_ITEMS_BASE, Gson().toJson(item))
+        activity?.let {
+            it.findViewById<View>(R.id.frameLayout)?.let {
+                /*Added By Rahul - Update,  for checking the update for the given item, This will be the base object, with which we will compare the updated object*/
+                prefProvider.setValue(Constants.OPEN_ORDER_ITEMS_BASE, Gson().toJson(item))
 
-        prefProvider.setValueInt(Constants.CAT_ID_SELECTED, item.categoryId)
-        val backStateName: String = AddItemFragment.javaClass.getName()
-        val fragment = AddItemFragment.newInstance(item, this, cartList, true, position)
-        val fm: FragmentManager = requireActivity().supportFragmentManager
-        fm.beginTransaction().add(binding.frameLayout.id, fragment).setReorderingAllowed(true)
-            .addToBackStack(backStateName).commit()
-        Log.e("Dashboard Tracking ", "Track Dashboard - 1664")
+                prefProvider.setValueInt(Constants.CAT_ID_SELECTED, item.categoryId)
+                val backStateName: String = AddItemFragment.javaClass.getName()
+                val fragment = AddItemFragment.newInstance(item, this, cartList, true, position)
+                val fm: FragmentManager = requireActivity().supportFragmentManager
+                fm.beginTransaction().add(binding.frameLayout.id, fragment).setReorderingAllowed(true)
+                    .addToBackStack(backStateName).commit()
+                Log.e("Dashboard Tracking ", "Track Dashboard - 1664")
+            }
+        }
+
     }
 
     override fun onDineInOrderCleared() {
