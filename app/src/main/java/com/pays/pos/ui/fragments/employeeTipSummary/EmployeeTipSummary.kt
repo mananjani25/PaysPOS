ee7312ca6477824166c87bfc5d5b41dae1e95fd2
ankit-internetsoft <ankit@internetsoft.site>
2025-03-11 16:15:18 +0530

    V1.39 PA1-I667 Solved

--- app/src/main/java/com/pays/pos/ui/fragments/employeeTipSummary/EmployeeTipSummary.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/employeeTipSummary/EmployeeTipSummary.kt
@@ -313,8 +313,8 @@ class EmployeeTipSummary : Fragment() {
         }
 
         endTime = TimePickerDialog.OnTimeSetListener { view, hour, minute ->
-            var fromDate = SimpleDateFormat("dd/MM/yyyy hh:mm a").parse(viewModel.startDate.value).getTime() / 1000
-            var endDate = SimpleDateFormat("dd/MM/yyyy hh:mm a").parse(timeCalculateForStartEndTime(hour, minute, "isend")).getTime() / 1000
+            var fromDate = SimpleDateFormat("MM/dd/yyyy hh:mm a").parse(viewModel.startDate.value).getTime() / 1000
+            var endDate = SimpleDateFormat("MM/dd/yyyy hh:mm a").parse(timeCalculateForStartEndTime(hour, minute, "isend")).getTime() / 1000
             if (fromDate<=endDate){
                 val timecalender = Calendar.getInstance()
                 timecalender.set(Calendar.HOUR_OF_DAY, hour)
