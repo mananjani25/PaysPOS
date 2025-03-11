992c786f71fbc22cb81c038b18f18aed933504c7
Aman Singh Chauhan <aman.internetsoft.co>
2025-03-11 17:30:34 +0530

    V1.39 PA1-I684 Solved.

--- app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -1228,10 +1228,12 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
 
 
                         var remainingValue = 0.0
+
                         remainingValue = if (cashDiscountType == "SurCharge") {
+                            val paymentAmount = paymentAmount - (cashDiscountSurcharge) - surchargeOnTip
                             wholePrice - String.format(
                                 "%.2f",
-                                paymentAmount - (cashDiscountSurcharge)
+                                paymentAmount
                             ).toDouble()
                         } else {
                             wholePrice - paymentAmount
@@ -1244,8 +1246,8 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
 
                          //to resolve tip before transaction issue
 
-                        if(isSelectedCount > 1)
-                        remainingValue += surchargeOnTip
+//                        if(isSelectedCount > 1)
+//                        remainingValue += surchargeOnTip
 
 
                         prefProvider.setValue(
