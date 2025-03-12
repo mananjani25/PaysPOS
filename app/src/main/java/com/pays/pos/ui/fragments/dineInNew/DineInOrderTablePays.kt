e42eacb6fa64fd15bba058d835b1768b19a6ee45
Aman Singh Chauhan <aman.internetsoft.co>
2025-03-12 22:31:58 +0530

    V1.36_dine_in PA1-I710 Solved.

--- app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
@@ -12493,7 +12493,10 @@ class DineInOrderTablePays : Fragment(), DineInTableAdapter.DineInTableListner {
                                     add(
                                         PrinterBuilder()
                                             .styleAlignment(Alignment.Left)
-                                            .actionPrintText(employee)
+                                            .styleMagnification(
+                                                MagnificationParameter(2, 2)
+                                            )
+                                            .actionPrintText(if (employee.length > 12) employee.take(21) + ".." else employee)
                                     )
                                     actionFeedLine(1)
                                 }
@@ -12501,6 +12504,9 @@ class DineInOrderTablePays : Fragment(), DineInTableAdapter.DineInTableListner {
                                 add(
                                     PrinterBuilder()
                                         .styleAlignment(Alignment.Left)
+                                        .styleMagnification(
+                                            MagnificationParameter(2, 2)
+                                        )
                                         .actionPrintText(
                                             orderTime
                                         )
