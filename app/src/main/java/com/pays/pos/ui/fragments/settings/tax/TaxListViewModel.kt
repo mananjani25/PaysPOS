36e9d2aa4474497b8409c38969bbb44579035dd3
Admin <rahul@internetsoft.site>
2025-03-26 16:55:35 +0530

    V1.43 PA1-I781_Solved

--- app/src/main/java/com/pays/pos/ui/fragments/settings/tax/TaxListViewModel.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/settings/tax/TaxListViewModel.kt
@@ -39,20 +39,26 @@ class TaxListViewModel @Inject constructor(
     val getTaxList = taxServiceChargeRepository.getTaxList()
 
     val taxList = MutableLiveData<List<TaxData>>()
+    /* Added by Rahul Pandit for PA1-I781*/
+    var shouldSyncData = true
+    /* Added by Rahul Pandit for PA1-I781*/
+
+
     fun setTaxData() {
         taxList.value = getTaxList.value?.data!!
     }
 
+    /* Added by Rahul Pandit for PA1-I781*/
     fun newTaxData() {
         viewModelScope.launch {
             val response = taxServiceChargeRepository.getTaxesList()
             if (response.status == Status.SUCCESS && response.data != null) {
-                _taxesData.value = Event(response.data)
-                taxList.value = response.data.data
                 taxServiceChargeRepository.addAllTaxListDatabase(response.data.data)
             }
         }
     }
+    /* Added by Rahul Pandit for PA1-I781*/
+
 
     fun getTextList() {
         _showProgress.value = Event(true)
