3aab057b5c87516cbeb75de30a9843d5c4fb05fe
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-04-01 17:11:55 +0530

    Dine In - Solved PA1-I651

--- app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/OrderCompleteFragment.kt
@@ -4652,9 +4652,11 @@ class OrderCompleteFragment : Fragment(), View.OnClickListener, StatusChangeEven
                                         val discountToPrint =
                                             padLine(
                                                 "Total Discount",
-                                                "-$" + MethodUtils.roundOffAmountString(
+                                                (if(guestDiscount != 0.0) "-$" + MethodUtils.roundOffAmountString(
                                                     guestDiscount
-                                                ),
+                                                ) else "$" + MethodUtils.roundOffAmountString(
+                                                    guestDiscount
+                                                )).toString(),
                                                 48
                                             ).toString()
 
