1484b0e41f5969e0ca2838873dd2026837a4c3b0
uday <uday@internetsoft.com>
2025-04-14 19:35:14 +0530

    Dine in  issue

--- app/src/main/java/com/pays/pos/ui/fragments/payment/PaymentViewModel.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/payment/PaymentViewModel.kt
@@ -312,10 +312,26 @@ open class PaymentViewModel @Inject constructor(
                                             }
                                         }
                                         else {
-                                            _showProgress.postValue(Event(false))
-                                            _data.value = Event(createOrderResponse)
 
-                                            tableCleared.value = true
+                                            _showProgress.postValue(Event(false))
+                                            if (response.data.order.payments[response.data.order.payments.size - 1].paymentType != "Card") {
+                                                cashLogApi(createOrderResponse, "in")
+                                            } else {
+                                                _data.value = Event(createOrderResponse)
+                                                EventBus.getDefault().post(
+                                                    MessageEvent(
+                                                        "${Constants.LINE_BREAK_TAB} PaymentViewModel.kt_line_3075 createOrderResponse -> ${
+                                                            Gson().toJson(createOrderResponse)
+                                                        }"
+                                                    )
+                                                )
+
+
+                                                tableCleared.value = true
+
+                                            }
+
+
 
                                             EventBus.getDefault().post(
                                                 MessageEvent(
