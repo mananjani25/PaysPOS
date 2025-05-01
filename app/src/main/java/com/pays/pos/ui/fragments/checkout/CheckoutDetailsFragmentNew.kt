631b19c1d43414232309914378465e41cdcf7fce
tripti <tripti@internetsoft.site>
2025-05-01 13:43:18 +0530

    PA1-I1030 - Resolve the issue.

--- app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/checkout/CheckoutDetailsFragmentNew.kt
@@ -6785,6 +6785,7 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
         binding.view3.setBackgroundColor(resources.getColor(R.color.backgroundColor))
         isSplitScreen = true
         isPaymentScreen = false
+        binding.enteredSplitAmount.text?.clear()
         binding.paymentLinearLayout.visibility = View.GONE
         binding.splitAmountLayout.visibility = View.GONE
         binding.splitLinearLayout.visibility = View.VISIBLE
@@ -6814,6 +6815,7 @@ class CheckoutDetailsFragmentNew(val isFromOpenOrder: Boolean = false) : Fragmen
         binding.view3.setBackgroundColor(resources.getColor(R.color.backgroundColor))
         isPaymentScreen = true
         isSplitScreen = false
+        binding.enteredSplitAmount.text?.clear()
         binding.paymentLinearLayout.visibility = View.VISIBLE
         binding.splitAmountLayout.visibility = View.GONE
         binding.splitLinearLayout.visibility = View.GONE
