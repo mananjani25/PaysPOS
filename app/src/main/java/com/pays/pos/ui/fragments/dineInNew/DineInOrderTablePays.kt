2fceedeacf64c9520bbefdce9c34d0b16d2e98ce
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-05-22 20:23:03 +0530

    Dine In - Solved PA1-I1115

--- app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/dineInNew/DineInOrderTablePays.kt
@@ -7304,7 +7304,7 @@ class DineInOrderTablePays : Fragment(), DineInTableAdapter.DineInTableListner {
 
                                     val subTotalToPrint = padLine(
                                         "Sub Total",
-                                        "$" + MethodUtils.roundOffAmountString(guestSubTotal - discountPriceForGuest),
+                                        "$" + MethodUtils.roundOffAmountString(subTotalGuest),
                                         if (customerSettingModel.fonts == Constants.LARGE) {
                                             23
                                         } else {
