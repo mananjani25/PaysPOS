520fbec6662d5212b6eb49b94142c2943f64efc5
ankit-internetsoft <ankit.internetsoft@gmail.com>
2025-06-10 20:54:27 +0530

    v1.50 solved PA1-I1170

--- app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
@@ -3200,6 +3200,14 @@ class TransactionDetailsFragment : Fragment() {
                         if (paymentDetailsResponse.data.order.note?.isNotEmpty() == true && kitchenSettingModel.showOrderNote == true) {
                             add(
                                 PrinterBuilder()
+                                    .styleBold(true)
+                                    .actionPrintText(
+                                        "--------------------------------------------"
+                                    )
+                            )
+                            actionFeedLine(1)
+                            add(
+                                PrinterBuilder()
                                     .styleMagnification(
                                         MagnificationParameter(2, 2)
                                     )
@@ -3207,7 +3215,7 @@ class TransactionDetailsFragment : Fragment() {
                                     .styleBold(true)
                                     .actionPrintText(
                                         content = if (paymentDetailsResponse.data.order.note?.isNotEmpty() == true && kitchenSettingModel.showOrderNote == true) {
-                                            "--------------------------------------------\nOrder Note\n "
+                                            "Order Note"
                                         } else ""
                                     )
                             )
@@ -3226,7 +3234,7 @@ class TransactionDetailsFragment : Fragment() {
                                     )
                             )
                         }
-                        actionFeedLine(1)
+                        actionFeedLine(2)
                         if (kitchenSettingModel.showCustomerName && (paymentDetailsResponse.data.order.customer?.firstName != null || paymentDetailsResponse.data.order.customer?.lastName != null)) {
                             add(
                                 PrinterBuilder()
@@ -5517,12 +5525,18 @@ class TransactionDetailsFragment : Fragment() {
                                     )
                                     lineBreak()
                                 }
-
+                                printDashedLineAndBreak()
+                                lineBreak()
 
                                 if (paymentDetailsResponse.data.order.note.isNotEmpty() == true && kitchenSettingModel.showOrderNote) {
 
+                                    printCenter(
+                                        "Order Note",
+                                        isBold = true,
+                                        fontSize = FONT_SIZE_5X
+                                    )
                                     lineBreak()
-
+                                    lineBreak()
                                     printCenter(
                                         paymentDetailsResponse.data.order.note.toString(),
                                         isBold = true,
@@ -5532,6 +5546,7 @@ class TransactionDetailsFragment : Fragment() {
 
                                 if (kitchenSettingModel.showCustomerAddress != false || kitchenSettingModel.showCustomerPhone != false || kitchenSettingModel.showCustomerName != false) {
                                     if (paymentDetailsResponse.data.order.customer != null) {
+                                        lineBreak()
                                         lineBreak()
                                         printLeft(
                                             "Customer Details", isBold = true,
