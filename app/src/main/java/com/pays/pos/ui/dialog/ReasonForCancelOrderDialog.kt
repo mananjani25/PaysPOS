2000ebe2910325c95044b6d661c12d20ae332709
tripti <tripti@internetsoft.site>
2025-06-12 14:34:11 +0530

    v1.49_PA1-I1166 - Resolve the issue.

--- app/src/main/java/com/pays/pos/ui/dialog/ReasonForCancelOrderDialog.kt
+++ app/src/main/java/com/pays/pos/ui/dialog/ReasonForCancelOrderDialog.kt
@@ -3243,7 +3243,7 @@ class ReasonForCancelOrderDialog : DialogFragment() {
 
 
                                     if (kitchenSettingModel.showTeamMember) {
-                                        printLeft("Employee:" + receiptModel?.employee?.name, isBold = true, fontSize = FONT_SIZE_5X)
+                                        printLeft("Employee:" + prefProvider.getValue(EMPLOYEE_NAME, ""), isBold = true, fontSize = FONT_SIZE_5X)
                                     }
                                     lineBreak()
 
