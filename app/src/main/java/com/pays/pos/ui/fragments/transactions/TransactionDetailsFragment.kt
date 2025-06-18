e81025a64a6925b58ed09aa959b540b37c64437f
Sushant Digraskar <digraskarsushant12@gmail.com>
2025-06-18 19:33:19 +0530

    v1.50 Tokenization - Solved PA1-I1209

--- app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
+++ app/src/main/java/com/pays/pos/ui/fragments/transactions/TransactionDetailsFragment.kt
@@ -1168,7 +1168,7 @@ class TransactionDetailsFragment : Fragment() {
                 tpn = prefProvider.getValue(
                     Constants.DEJAVOO_TPN,""
                 ),
-                amount = (paymentDetailsResponse.data.amount * 100).toInt().toString(),
+                amount = ((paymentDetailsResponse.data.amount + + paymentDetailsResponse.data.tips) * 100).toInt().toString(),
                 isProd = Constants.paymentLive,
                 paymentType = "Credit",
                 performedBy = "",
@@ -1197,7 +1197,7 @@ class TransactionDetailsFragment : Fragment() {
                                 )
                             ) {
                                 CoroutineScope(Dispatchers.Main).launch {
-                                    refundCall(paymentDetailsResponse.data.amount)
+                                    refundCall(paymentDetailsResponse.data.amount + paymentDetailsResponse.data.tips)
                                 }
                             } else {
                                 AlertUtils.showCustomAlert(
